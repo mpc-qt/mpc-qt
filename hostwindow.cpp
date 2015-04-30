f3b27e04df5aa573fddff521fc18db7e983add90
commander kotori <cmdrkotori@gmail.com>
2015-04-30 23:33:50 +1000

    about: fix spacing at eol

--- hostwindow.cpp
+++ hostwindow.cpp
@@ -10,6 +10,7 @@
 #include <QFileDialog>
 #include <QTime>
 #include <QMessageBox>
+#include <QLibraryInfo>
 #include <QDebug>
 
 HostWindow::HostWindow(QWidget *parent) :
@@ -262,19 +263,20 @@ void HostWindow::menu_help_about()
       "<h3>LICENSE</h3>"
       "<p>    Copyright (C) 2015"
       "<p>"
-      "This program is free software; you can redistribute it and/or modify"
-      "it under the terms of the GNU General Public License as published by"
-      "the Free Software Foundation; either version 2 of the License, or"
+      "This program is free software; you can redistribute it and/or modify "
+      "it under the terms of the GNU General Public License as published by "
+      "the Free Software Foundation; either version 2 of the License, or "
       "(at your option) any later version."
       "<p>"
-      "This program is distributed in the hope that it will be useful,"
-      "but WITHOUT ANY WARRANTY; without even the implied warranty of"
-      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+      "This program is distributed in the hope that it will be useful, "
+      "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
       "GNU General Public License for more details."
       "<p>"
-      "You should have received a copy of the GNU General Public License along"
-      "with this program; if not, write to the Free Software Foundation, Inc.,"
+      "You should have received a copy of the GNU General Public License along "
+      "with this program; if not, write to the Free Software Foundation, Inc., "
       "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.");
+
 }
 
 void HostWindow::build_menu()
