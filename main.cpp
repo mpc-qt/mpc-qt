0dca7f0afe73ac0e84951660b2ff11376d52b5cb
commander kotori <cmdrkotori@gmail.com>
2015-06-10 20:06:44 +1000

    main: connect mainwindow to playlist manager object
    There are of course several things that still need to be ported, such as
    the mpvwidget signals that the playbackmanager has to control.

--- main.cpp
+++ main.cpp
@@ -21,6 +21,42 @@ Flow::Flow(QObject *owner) :
     mainWindow = new MainWindow();
     playbackManager = new PlaybackManager(this);
     playbackManager->setMpvWidget(mainWindow->mpvWidget(), true);
+
+    connect(mainWindow, SIGNAL(filesOpened(QList<QUrl>)),
+            playbackManager, SLOT(openFiles(QList<QUrl>)));
+    connect(mainWindow, SIGNAL(paused()),
+            playbackManager, SLOT(pausePlayer()));
+    connect(mainWindow, SIGNAL(unpaused()),
+            playbackManager, SLOT(unpausePlayer()));
+    connect(mainWindow, &MainWindow::stepBackward,
+            playbackManager, &PlaybackManager::stepBackward);
+    connect(mainWindow, &MainWindow::stepForward,
+            playbackManager, &PlaybackManager::stepForward);
+    connect(mainWindow, &MainWindow::speedDown,
+            playbackManager, &PlaybackManager::speedDown);
+    connect(mainWindow, &MainWindow::speedUp,
+            playbackManager, &PlaybackManager::speedUp);
+    //connect(mainWindow, &MainWindow::speedReset,
+    //        playbackManager, &PlaybackManager::speedReset);
+    connect(mainWindow, &MainWindow::audioTrackSelected,
+            playbackManager, &PlaybackManager::setAudioTrack);
+    connect(mainWindow, &MainWindow::subtitleTrackSelected,
+            playbackManager, &PlaybackManager::setSubtitleTrack);
+    connect(mainWindow, &MainWindow::videoTrackSelected,
+            playbackManager, &PlaybackManager::setVideoTrack);
+    connect(mainWindow, &MainWindow::volumeChanged,
+            playbackManager, &PlaybackManager::setVolume);
+    connect(mainWindow, &MainWindow::volumeMuteChanged,
+            playbackManager, &PlaybackManager::setMute);
+    connect(mainWindow, &MainWindow::chapterPrevious,
+            playbackManager, &PlaybackManager::navigateToPrevChapter);
+    connect(mainWindow, &MainWindow::chapterNext,
+            playbackManager, &PlaybackManager::navigateToNextChapter);
+    connect(mainWindow, &MainWindow::chapterSelected,
+            playbackManager, &PlaybackManager::navigateToChapter);
+    connect(mainWindow, &MainWindow::timeSelected,
+            playbackManager, &PlaybackManager::navigateToTime);
+
 }
 
 Flow::~Flow()
