c9d760f4b2090454e416a12575e87868bb57aefa
commander kotori <cmdrkotori@gmail.com>
2015-09-06 22:37:27 +1000

    playlistwindow: add quick playlist in constructor
    Ensures that the null (quick) playlist is present.  Doesn't do anything
    until it gets hooked up.

--- playlistwindow.cpp
+++ playlistwindow.cpp
@@ -1,11 +1,15 @@
 #include "playlistwindow.h"
 #include "ui_playlistwindow.h"
+#include "qdrawnplaylist.h"
 
 PlaylistWindow::PlaylistWindow(QWidget *parent) :
     QDockWidget(parent),
     ui(new Ui::PlaylistWindow)
 {
     ui->setupUi(this);
+    auto qdp = new QDrawnPlaylist();
+    qdp->setUuid(QUuid());
+    ui->tabWidget->addTab(qdp, tr("Quick Playlist"));
 }
 
 PlaylistWindow::~PlaylistWindow()
