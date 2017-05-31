74923fc3b60606d175b5bda5f7d8a5b258d20f29
luigino <aloisio@gmx.com>
2017-05-31 10:55:44 +0200

    Set build date only once

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1672,12 +1672,13 @@ void MainWindow::on_actionHelpHomepage_triggered()
 
 void MainWindow::on_actionHelpAbout_triggered()
 {
+    QDateTime buildDate = QDateTime::currentDateTimeUtc();
     QMessageBox::about(this, tr("About Media Player Classic Qute Theater"),
       "<h2>" + tr("Media Player Classic Qute Theater") + "</h2>" +
       "<p>" + tr("A clone of Media Player Classic written in Qt") +
       "<p>" + tr("Based on Qt ") + QT_VERSION_STR + tr(" and ") + mpvw->mpvVersion() +
-      "<p>" + tr("Built on ") + QDateTime::currentDateTimeUtc().date().toString(Qt::DefaultLocaleShortDate) +
-              tr(" at ") + QDateTime::currentDateTimeUtc().time().toString(Qt::DefaultLocaleShortDate) +
+      "<p>" + tr("Built on ") + buildDate.date().toString(Qt::DefaultLocaleShortDate) +
+              tr(" at ") + buildDate.time().toString(Qt::DefaultLocaleShortDate) +
       "<h3>LICENSE</h3>"
       "<p>   Copyright (C) 2015"
       "<p>"
