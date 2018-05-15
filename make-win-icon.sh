e2631bbed24c4d9ff5bc03d9ed99664a0cabf896
Kotori Itsuka <cmdrkotori@gmail.com>
2018-05-15 04:21:57 +0000

    make-win-icon: speed up build
    Only recreate the icon if it does not exist.

--- make-win-icon.sh
+++ make-win-icon.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-(magick convert -quiet images/icon/mpc-qt.svg -define icon:auto-resize=16,48,256 -compress zip mpc-qt.ico)2>&1>&/dev/null
+if [ ! -f mpc-qt.ico ]; then
+    (magick convert -quiet images/icon/mpc-qt.svg -define icon:auto-resize=16,48,256 -compress zip mpc-qt.ico)2>&1>&/dev/null
+fi
 echo mpc-qt.ico
