d2c005540d5f776a86d449f15d7b215c3175877d
Kotori Itsuka <cmdrkotori2@gmail.com>
2020-07-04 13:49:40 +1000

    settingswindow: redo previous commit
    Hashes were inserted twice due to a copy-paste error.

--- settingswindow.cpp
+++ settingswindow.cpp
@@ -513,19 +513,7 @@ void SettingsWindow::generateVideoPresets()
     videoWidgets.unite(generateSettingMap(ui->debandTab));
     videoWidgets.unite(generateSettingMap(ui->syncMode));
 
-    videoWidgets.unite(generateSettingMap(ui->generalTab));
-    videoWidgets.unite(generateSettingMap(ui->ditherTab));
-    videoWidgets.unite(generateSettingMap(ui->scalingTab));
-    videoWidgets.unite(generateSettingMap(ui->debandTab));
-    videoWidgets.unite(generateSettingMap(ui->syncMode));
-
     // Non-deprecated way since Qt 5.15
-    //videoWidgets.insert(generateSettingMap(ui->generalTab));
-    //videoWidgets.insert(generateSettingMap(ui->ditherTab));
-    //videoWidgets.insert(generateSettingMap(ui->scalingTab));
-    //videoWidgets.insert(generateSettingMap(ui->debandTab));
-    //videoWidgets.insert(generateSettingMap(ui->syncMode));
-    //
     //videoWidgets.insert(generateSettingMap(ui->generalTab));
     //videoWidgets.insert(generateSettingMap(ui->ditherTab));
     //videoWidgets.insert(generateSettingMap(ui->scalingTab));
