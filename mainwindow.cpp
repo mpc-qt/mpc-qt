b75caa5f9e67309025757c57173f98f0a528ec42
Kotori Itsuka <cmdrkotori2@gmail.com>
2021-06-10 10:03:04 +1000

    mainwindow: use new website
    It ain't much, but it's honest work.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2196,7 +2196,7 @@ void MainWindow::on_actionNavigateChaptersNext_triggered()
 
 void MainWindow::on_actionHelpHomepage_triggered()
 {
-    QDesktopServices::openUrl(QUrl("https://github.com/cmdrkotori/mpc-qt-origin"));
+    QDesktopServices::openUrl(QUrl("https://cmdrkotori.github.io/mpc-qt.html"));
 }
 
 void MainWindow::on_actionHelpAbout_triggered()
