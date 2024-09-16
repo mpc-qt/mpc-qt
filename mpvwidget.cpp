mpc-qt/mpc-qt

commit d37b4a1e279110e49de308ae8ff0f6817a69fc5e

Reduce property change logging to 3 decimals
Don't display any decimal if those are zero.

mpvwidget.cpp
@@ -632,6 +632,12 @@ void MpvObject::hideCursor()
 
 void MpvObject::ctrl_mpvPropertyChanged(QString name, QVariant v)
 {
+    // Don't show more than 3 decimals or none if those are zero
+    if (v.canConvert(QMetaType(QMetaType::Double))) {
+        v = QString("%1").arg(QString::number(v.toDouble(), 'f', 3));
+        if (v.toString().section('.', 1) == "000")
+            v = v.toString().section('.', 0, 0);
+    }
     if (debugMessages)
         LogStream("mpvobject") << name << " property changed to " << v;
 

commit 391140eaac9901848de25970f67d6e33dc144b4d

Fix type detection in MpvObject::ctrl_mpvPropertyChanged
Fixes regression caused by d37b4a1e279110e49de308ae8ff0f6817a69fc5e
"Reduce property change logging to 3 decimals".

mpvwidget.cpp
@@ -633,7 +633,7 @@ void MpvObject::hideCursor()
 void MpvObject::ctrl_mpvPropertyChanged(QString name, QVariant v)
 {
     // Don't show more than 3 decimals or none if those are zero
-    if (v.canConvert(QMetaType(QMetaType::Double))) {
+    if (v.typeId() == QVariant::Double) {
         v = QString("%1").arg(QString::number(v.toDouble(), 'f', 3));
         if (v.toString().section('.', 1) == "000")
             v = v.toString().section('.', 0, 0);