mpc-qt/mpc-qt

commit 509927bc3a8f41f507e2277346fa0e849096fd0f

manager: fix early finish
Fake a file end event when a file does not properly initialize.

Consider the case where a stream is opened but fails to properly
initialize such as the url https://chiru.no:8000/stream.mp3.  In these
scenarios libmpv would finish playback before it starts, and the player
would be stuck in the Buffering state.  We need to progress to the next
file in the playlist.  We need to get out of the Buffering state when
there is no file after us in the playlist.  So send a stopped state
event and jump to the code that handles an end of file event.  This
mimics the behviour of mpc-hc from my tests.

manager.cpp
@@ -70,6 +70,8 @@ void PlaybackManager::setMpvObject(MpvObject *mpvObject, bool makeConnections)
                 this, &PlaybackManager::mpvw_pausedChanged);
         connect(mpvObject, &MpvObject::playbackIdling,
                 this, &PlaybackManager::mpvw_playbackIdling);
+        connect(mpvObject, &MpvObject::playbackFinished,
+                this, &PlaybackManager::mpvw_playbackFinished);
         connect(mpvObject, &MpvObject::eofReachedChanged,
                 this, &PlaybackManager::mpvw_eofReachedChanged);
         connect(mpvObject, &MpvObject::mediaTitleChanged,
@@ -837,6 +839,14 @@ void PlaybackManager::mpvw_playbackIdling()
         playbackState_ = StoppedState;
         emit stateChanged(playbackState_);
         return;
+    }
+}
+
+void PlaybackManager::mpvw_playbackFinished() {
+    if (playbackState_ == BufferingState || playbackState_ == WaitingState) {
+        playbackState_ = StoppedState;
+        emit stateChanged(playbackState_);
+        mpvw_eofReachedChanged(true);
     }
 }
 

manager.h
@@ -176,6 +176,7 @@ private slots:
     void mpvw_playbackStarted();
     void mpvw_pausedChanged(bool yes);
     void mpvw_playbackIdling();
+    void mpvw_playbackFinished();
     void mpvw_eofReachedChanged(bool eof);
     void mpvw_mediaTitleChanged(QString title);
     void mpvw_chapterDataChanged(QVariantMap metadata);

commit eca033b1b9f91957dd073fcfd6b7393fb9637c0c

manager: fix file progression
Don't bail out when the file is loading but hasn't buffered yet.  Due to
a race condition, files could be skipped at eof.  For example, tracks 1
and 4 and 9 would be played in the playlist instead of 1 2 3 4 and so
on.

Fixes regression introduced in 509927b.

manager.cpp
@@ -816,7 +816,7 @@ void PlaybackManager::mpvw_playbackIdling()
 }
 
 void PlaybackManager::mpvw_playbackFinished() {
-    if (playbackState_ == BufferingState || playbackState_ == WaitingState) {
+    if (playbackState_ == BufferingState) {
         playbackState_ = StoppedState;
         emit stateChanged(playbackState_);
         mpvw_eofReachedChanged(strTrue);