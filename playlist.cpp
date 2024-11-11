mpc-qt/mpc-qt

commit efceb5e1da37550e51195fb74d2aea9de3557679

Implement full playlist shuffle mode
When shuffle mode is enabled, the original positions of the playlist
items are saved, and the playlist is visibly shuffled. Each item is only
played once and the next item is the item below the current item.
The currently playing item is kept as the first item in shuffle mode.
Once shuffle mode is disabled, the items are restored to their original
positions.
The Randomize feature is replaced by a Reshuffle feature in the shuffle
mode.
The Sort and Restore features are disabled in shuffle mode.
When the user adds items, they are added to the end of the playlist.

playlist.cpp
@@ -2,6 +2,7 @@
 #include <QMutableListIterator>
 #include <cmath>
 #include "playlist.h"
+#include <random>
 
 
 
@@ -15,6 +16,7 @@ static char keyShuffle[] = "shuffle";
 static char keyTitle[] = "title";
 static char keyUrl[] = "url";
 static char keyUuid[] = "uuid";
+static char keyOriginalPosition[] = "originalposition";
 
 
 
@@ -69,7 +71,7 @@ void Item::setMetadata(const QVariantMap &qvm)
     metadata_ = qvm;
 }
 
-int Item::originalPosition()
+int Item::originalPosition() const
 {
     return originalPosition_;
 }
@@ -154,6 +156,8 @@ QVariantMap Item::toVMap() const
     v.insert(keyUrl, url());
     v.insert(keyUuid, uuid());
     v.insert(keyMetadata, metadata());
+    if (PlaylistCollection::getSingleton()->getPlaylist(playlistUuid())->shuffle())
+        v.insert(keyOriginalPosition, originalPosition());
     return v;
 }
 
@@ -162,6 +166,8 @@ void Item::fromVMap(const QVariantMap &qvm)
     url_ = qvm.contains(keyUrl) ? qvm.value(keyUrl).toUrl() : QUrl();
     itemUuid_ = qvm.contains(keyUuid) ? qvm.value(keyUuid).toUuid() : QUuid::createUuid();
     metadata_ = qvm.contains(keyMetadata) ? qvm.value(keyMetadata).toMap() : QVariantMap();
+    if (qvm.contains(keyOriginalPosition))
+        originalPosition_ = qvm.value(keyOriginalPosition).toInt();
 }
 
 QSharedPointer<ItemCollection> ItemCollection::collection;
@@ -446,6 +452,29 @@ bool Playlist::shuffle()
 void Playlist::setShuffle(bool shuffling)
 {
     shuffle_ = shuffling;
+    if (shuffling)
+        shuffleItems();
+    else
+        unshuffleItems();
+}
+
+void Playlist::shuffleItems()
+{
+    QWriteLocker locker(&listLock);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(items.begin(), items.end(), gen);
+    locker.unlock();
+    this->setNowPlaying(items.first()->uuid());
+}
+
+void Playlist::unshuffleItems()
+{
+    QWriteLocker locker(&listLock);
+    std::sort(items.begin(), items.end(),
+        [](const QSharedPointer<Item> &a, const QSharedPointer<Item> &b) {
+            return a->originalPosition() < b->originalPosition();
+    });
 }
 
 QUuid Playlist::uuid()
@@ -537,6 +566,11 @@ void Playlist::fromVMap(const QVariantMap &qvm)
             this->items.append(i);
             this->itemsByUuid.insert(i->uuid(), i);
             ItemCollection::getSingleton()->storeItem(i);
+        }
+        // Reshuffle on first start with non shuffled list in shuffle mode
+        if (shuffle() && !items.isEmpty() && !items[0].toMap().contains(keyOriginalPosition)) {
+            locker.unlock();
+            shuffleItems();
         }
     }
 }

playlist.h
@@ -34,7 +34,7 @@ public:
     const QVariantMap &metadata() const;
     void setMetadata(const QVariantMap &qvm);
 
-    int originalPosition();
+    int originalPosition() const;
     void setOriginalPosition(int i);
 
     int queuePosition() const;
@@ -124,6 +124,8 @@ public:
     void setRepeat(bool repeat);
     bool shuffle();
     void setShuffle(bool shuffle);
+    void shuffleItems();
+    void unshuffleItems();
     QUuid uuid();
     void setUuid(const QUuid &playlistUuid);
     QUuid nowPlaying();

playlistwindow.cpp
@@ -16,9 +16,7 @@
 PlaylistWindow::PlaylistWindow(QWidget *parent) :
     QDockWidget(parent),
     ui(new Ui::PlaylistWindow),
-    currentPlaylist(),
-    randomDevice(),
-    randomGenerator(randomDevice())
+    currentPlaylist()
 {
     clipboard = new PlaylistSelection;
 
@@ -133,13 +131,8 @@ PlaylistItem PlaylistWindow::getItemAfter(QUuid list, QUuid item)
     if (!next.item.isNull())
         return next;
     QSharedPointer<Item> after;
-    if (pl->shuffle() && !pl->isEmpty()) {
-        std::uniform_int_distribution<> itemDistribution(0, pl->count()-1);
-        after = pl->itemAt(itemDistribution(randomGenerator));
-    } else {
-        after = pl->itemAfter(item);
-    }
-    if (!after || (after->uuid() == item && pl->count() < 2))
+    after = pl->itemAfter(item);
+    if (!after)
         return { QUuid(), QUuid() };
     return { pl->uuid(), after->uuid() };
 }
@@ -749,20 +742,27 @@ void PlaylistWindow::sortPlaylistByUrl(const QUuid &playlistUuid)
     qdp->sort<QString>(converter, lessThan);
 }
 
-void PlaylistWindow::randomizePlaylist(const QUuid &playlistUuid)
+void PlaylistWindow::shufflePlaylist(const QUuid &playlistUuid, bool shuffle)
+{
+    if (widgets.contains(playlistUuid))
+        widgets[playlistUuid]->playlist()->setShuffle(shuffle);
+    refreshPlaylist(playlistUuid);
+}
+
+void PlaylistWindow::reshufflePlaylist(const QUuid &playlistUuid)
+{
+    if (widgets.contains(playlistUuid))
+        widgets[playlistUuid]->playlist()->shuffleItems();
+    refreshPlaylist(playlistUuid);
+}
+
+void PlaylistWindow::refreshPlaylist(const QUuid &playlistUuid)
 {
     auto qdp = widgets.value(playlistUuid, nullptr);
-    if (!qdp)
-        return;
-    std::uniform_int_distribution<> itemDistribution(0, qdp->count()-1);
-    auto converter = [&](QSharedPointer<Item> i) {
-        Q_UNUSED(i)
-        return itemDistribution(randomGenerator);
-    };
-    auto lessThan = [](const int &a, const int &b) {
-        return a < b;
-    };
-    qdp->sort<int>(converter, lessThan);
+    if (qdp) {
+        qdp->repopulateItems();
+        qdp->setCurrentItem(widgets[playlistUuid]->playlist()->nowPlaying());
+    }
 }
 
 void PlaylistWindow::restorePlaylist(const QUuid &playlistUuid)
@@ -842,6 +842,7 @@ void PlaylistWindow::playlist_contextMenuRequested(const QPoint &p, const QUuid 
     if (!widgets.contains(playlistUuid))
         return;
     DrawnPlaylist *listWidget = widgets[playlistUuid];
+    auto pl = PlaylistCollection::getSingleton()->getPlaylist(playlistUuid);
 
     QMenu *m = new QMenu(this);
     QAction *a;
@@ -902,6 +903,8 @@ void PlaylistWindow::playlist_contextMenuRequested(const QPoint &p, const QUuid 
             this, [this,playlistUuid]() {
         sortPlaylistByLabel(playlistUuid);
     });
+    if (pl && pl->shuffle())
+        a->setDisabled(true);
     m->addAction(a);
 
     a = new QAction(m);
@@ -910,14 +913,8 @@ void PlaylistWindow::playlist_contextMenuRequested(const QPoint &p, const QUuid 
             this, [this,playlistUuid]() {
         sortPlaylistByUrl(playlistUuid);
     });
-    m->addAction(a);
-
-    a = new QAction(m);
-    a->setText(tr("Randomize"));
-    connect(a, &QAction::triggered,
-            this, [this,playlistUuid]() {
-        randomizePlaylist(playlistUuid);
-    });
+    if (pl && pl->shuffle())
+        a->setDisabled(true);
     m->addAction(a);
 
     a = new QAction(m);
@@ -926,6 +923,18 @@ void PlaylistWindow::playlist_contextMenuRequested(const QPoint &p, const QUuid 
             this, [this,playlistUuid]() {
         restorePlaylist(playlistUuid);
     });
+    if (pl && pl->shuffle())
+        a->setDisabled(true);
+    m->addAction(a);
+
+    a = new QAction(m);
+    a->setText(tr("Reshuffle"));
+    connect(a, &QAction::triggered,
+            this, [this,playlistUuid]() {
+                reshufflePlaylist(playlistUuid);
+    });
+    if (!pl || !pl->shuffle())
+        a->setDisabled(true);
     m->addAction(a);
 
     m->addSeparator();
@@ -948,9 +957,8 @@ void PlaylistWindow::playlist_contextMenuRequested(const QPoint &p, const QUuid 
     a->setChecked(listWidget->playlist()->shuffle());
     connect(a, &QAction::triggered,
             this, [this,playlistUuid](bool checked) {
-        if (widgets.contains(playlistUuid))
-            widgets[playlistUuid]->playlist()->setShuffle(checked);
-        emit playlistShuffleChanged(playlistUuid, checked);
+                shufflePlaylist(playlistUuid, checked);
+                emit playlistShuffleChanged(playlistUuid, checked);
     });
     m->addAction(a);
 

playlistwindow.h
@@ -4,7 +4,6 @@
 #include <QDockWidget>
 #include <QHash>
 #include <QUuid>
-#include <random>
 #include "helpers.h"
 #include "playlist.h"
 
@@ -124,7 +123,9 @@ private slots:
     void savePlaylist(const QUuid &playlistUuid);
     void sortPlaylistByLabel(const QUuid &playlistUuid);
     void sortPlaylistByUrl(const QUuid &playlistUuid);
-    void randomizePlaylist(const QUuid &playlistUuid);
+    void shufflePlaylist(const QUuid &playlistUuid, bool shuffle);
+    void reshufflePlaylist(const QUuid &playlistUuid);
+    void refreshPlaylist(const QUuid & playlistUuid);
     void restorePlaylist(const QUuid &playlistUuid);
 
     void self_visibilityChanged();
@@ -158,8 +159,6 @@ private:
     QHash<QUuid, DrawnPlaylist*> widgets;
     DrawnPlaylist* queueWidget = nullptr;
     PlaylistSelection *clipboard = nullptr;
-    std::random_device randomDevice;
-    std::mt19937 randomGenerator;
 };
 
 #endif // PLAYLISTWINDOW_H

widgets/drawnplaylist.h
@@ -76,6 +76,8 @@ public:
 
     void setFilter(QString needles);
 
+    void repopulateItems();
+
 protected:
     bool event(QEvent *e);
 
@@ -101,8 +103,6 @@ signals:
     void contextMenuRequested(QPoint p, QUuid playlistUuid, QUuid itemUuid);
 
 private slots:
-    void repopulateItems();
-
     void model_rowsMoved(const QModelIndex & parent, int start, int end,
                          const QModelIndex & destination, int row);
     void self_currentItemChanged(QListWidgetItem *current,

commit f9bc20e5f4d5303cc3e90c8e7f1f7ff10ab473fd

Check if playlist pointer is valid before accessing shuffle property
This crash happens on exit when playlist backups don't already contain
the repeat attribute.
This check means that until backups get updated (and thus contain
repeat) by removing or restoring a playlist, they won't get written to
disk.
Regression caused by efceb5e1da37550e51195fb74d2aea9de3557679.

playlist.cpp
@@ -156,8 +156,9 @@ QVariantMap Item::toVMap() const
     v.insert(keyUrl, url());
     v.insert(keyUuid, uuid());
     v.insert(keyMetadata, metadata());
-    if (PlaylistCollection::getSingleton()->getPlaylist(playlistUuid())->shuffle())
-        v.insert(keyOriginalPosition, originalPosition());
+    if (PlaylistCollection::getSingleton()->getPlaylist(playlistUuid()) &&
+        PlaylistCollection::getSingleton()->getPlaylist(playlistUuid())->shuffle())
+            v.insert(keyOriginalPosition, originalPosition());
     return v;
 }
 