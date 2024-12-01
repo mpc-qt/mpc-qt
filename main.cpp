dbc923a089509b3066bf6f0a94581550836f87f4
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2024-12-01 13:08:44 +0100

    Save position of current file when playing new one from playlist
    When we start playing a file from a playlist,
    PlaybackManager::startPlayWithUuid gets directly triggered, so we need
    to save the position when that happens.
    
    We need to check if the url isn't empty because it gets reset when we
    open the same file again from the playlist, so it would add an empty
    entry in recent files.
    
    We could probably stop calling Flow::updateRecentPosition in
    Flow::manager_playLengthChanged if we call updateRecentPosition again at
    the end of Flow::manager_startingPlayingFile, but it'll require more
    testing to avoid regressions.

--- main.cpp
+++ main.cpp
@@ -1419,6 +1419,7 @@ void Flow::manager_openingNewFile()
 void Flow::manager_startingPlayingFile(QUrl url)
 {
     if (rememberFilePosition) {
+        updateRecentPosition(false);
         // Check if there's a position saved in recents for this file
         foreach (TrackInfo track, recentFiles) {
             if (track.url == url) {
@@ -1587,7 +1588,7 @@ void Flow::updateRecentPosition(bool resetPosition)
         resetPosition = true;
     playbackManager->getCurrentTrackInfo(url, listUuid, itemUuid, title, length, position,
                                          videoTrack, audioTrack, subtitleTrack, hasVideo);
-    if (!itemUuid.isNull() && (hasVideo || !rememberHistoryOnlyForVideos))
+    if (!itemUuid.isNull() && url.isLocalFile() && (hasVideo || !rememberHistoryOnlyForVideos))
         updateRecents(url, listUuid, itemUuid, title, length, resetPosition ? 0 : position,
                       videoTrack, audioTrack, subtitleTrack);
 }
