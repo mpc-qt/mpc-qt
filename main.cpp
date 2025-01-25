mpc-qt/mpc-qt

commit f8c7aab2d405d6e15538aff96d582a783a02fefa

Save position of current file when playing new one from playlist
When we start playing a file from a playlist,
PlaybackManager::startPlayWithUuid gets directly triggered, so we need
to save the position when that happens.

We need to check if the url isn't empty because it gets reset when we
open the same file again from the playlist, so it would add an empty
entry in recent files.

We could probably stop calling Flow::updateRecentPosition in
Flow::manager_playLengthChanged if we call updateRecentPosition again at
the end of Flow::manager_startingPlayingFile, but it'll require more
testing to avoid regressions.

main.cpp
@@ -1427,6 +1427,7 @@ void Flow::manager_openingNewFile()
 void Flow::manager_startingPlayingFile(QUrl url)
 {
     if (rememberFilePosition) {
+        updateRecentPosition(false);
         // Check if there's a position saved in recents for this file
         foreach (TrackInfo track, recentFiles) {
             if (track.url == url) {
@@ -1595,7 +1596,7 @@ void Flow::updateRecentPosition(bool resetPosition)
         resetPosition = true;
     playbackManager->getCurrentTrackInfo(url, listUuid, itemUuid, title, length, position,
                                          videoTrack, audioTrack, subtitleTrack, hasVideo);
-    if (!itemUuid.isNull() && (hasVideo || !rememberHistoryOnlyForVideos))
+    if (!itemUuid.isNull() && url.isLocalFile() && (hasVideo || !rememberHistoryOnlyForVideos))
         updateRecents(url, listUuid, itemUuid, title, length, resetPosition ? 0 : position,
                       videoTrack, audioTrack, subtitleTrack);
 }

commit 7a3907aa57ed598b987f154173e80ea94e9b85d5

Don't set nowPlayingItem in playNextFileUrl
It already gets set in startPlayWithUuid.
Otherwise, when calling updateRecentPosition in
manager_startingPlayingFile, the next file gets the position of the
current file.

Fixes regression caused by f8c7aab2d405d6e15538aff96d582a783a02fefa
Fixes #332

main.cpp
@@ -1428,6 +1428,7 @@ void Flow::manager_openingNewFile()
 
 void Flow::manager_startingPlayingFile(QUrl url)
 {
+    Logger::log("main", "manager_startingPlayingFile");
     if (firstFile) {
         firstFile = false;
         mainWindow->fixMpvwSize();

manager.cpp
@@ -756,8 +756,8 @@ bool PlaybackManager::playNextFileUrl(QUrl url, int delta)
     } while (!Helpers::urlSurvivesFilter(url, true));
     emit playingNextFile();
     playlistWindow_->clearPlaylist(nowPlayingList);
-    nowPlayingItem = playlistWindow_->addToPlaylist(nowPlayingList, { url }).item;
-    startPlayWithUuid(url, nowPlayingList, nowPlayingItem, false);
+    QUuid nowPlayingItemLocal = playlistWindow_->addToPlaylist(nowPlayingList, { url }).item;
+    startPlayWithUuid(url, nowPlayingList, nowPlayingItemLocal, false);
     return true;
 }
 