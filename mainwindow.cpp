mpc-qt/mpc-qt

commit 7e8ea0289a023d13fa909a4413e72b2dc6a8bb07

Remove audio volume from Options and move balance to audio page
Volume setting isn't very useful in the Options as it is usually set in
the main window.
And since 0e84a7fef2a2eb7bc40618e63a2b3816535503bd, when we open the
Playback settings, the volume has the same value as during the app
start.
This means that when we apply or close the Options with Ok, the main
window volume slider gets set to the settings volume.

Fixes #291.

mainwindow.cpp
@@ -183,6 +183,7 @@ QVariantMap MainWindow::state()
         { WRAP(ui->actionViewFullscreen) },
         { WRAP(ui->actionPlaySubtitlesEnabled) },
         { WRAP(ui->actionPlayVolumeMute) },
+        { "volume", volumeSlider_->value() },
         { "shownStatsPage", mpvObject_->selectedStatsPage() }
     };
 #undef WRAP
@@ -216,6 +217,8 @@ void MainWindow::setState(const QVariantMap &map)
 
     on_actionPlaySubtitlesEnabled_triggered(ui->actionPlaySubtitlesEnabled->isChecked());
     on_actionPlayVolumeMute_toggled(ui->actionPlayVolumeMute->isChecked(), true);
+    volumeSlider_->setValue(map.value("volume", 100).toInt());
+    volume_sliderMoved(volumeSlider_->value());
     setOSDPage(map.value("shownStatsPage",0).toInt());
     updateOnTop();
 

main.cpp
@@ -642,8 +642,6 @@ void Flow::setupSettingsConnections()
 {
 
     // mainwindow -> settings
-    connect(mainWindow, &MainWindow::volumeChanged,
-            settingsWindow, &SettingsWindow::setVolume);
     connect(mainWindow, &MainWindow::zoomPresetChanged,
             settingsWindow, &SettingsWindow::setZoomPreset);
 

settingswindow.cpp
@@ -356,7 +356,6 @@ SettingsWindow::SettingsWindow(QWidget *parent) :
 
     setupPageTree();
     setupColorPickers();
-    setupSelfSignals();
     setupUnimplementedWidgets();
 }
 
@@ -455,12 +454,6 @@ void SettingsWindow::setupFullscreenCombo()
     screenCombo = new ScreenCombo(this);
     screenCombo->setObjectName("fullscreenMonitor");
     ui->fullscreenMonitorLayout->addWidget(screenCombo);
-}
-
-void SettingsWindow::setupSelfSignals()
-{
-    connect(this, &SettingsWindow::volumeMax,
-            this, &SettingsWindow::self_volumeMax);
 }
 
 void SettingsWindow::setupUnimplementedWidgets()
@@ -745,10 +738,8 @@ void SettingsWindow::sendSignals()
     emit webserverRoot(WIDGET_PLACEHOLD_LOOKUP(ui->webRoot));
     emit webserverDefaultPage(WIDGET_PLACEHOLD_LOOKUP(ui->webDefaultPage));
 
-    int vol = WIDGET_LOOKUP(ui->playbackVolume).toInt();
     int volmax = WIDGET_LOOKUP(ui->tweaksVolumeLimit).toBool() ? 100 : 130;
     emit volumeMax(volmax);
-    emit volume(std::min(vol, volmax), true);
     emit volumeStep(WIDGET_LOOKUP(ui->playbackVolumeStep).toInt());
     {
         int i = WIDGET_LOOKUP(ui->playbackSpeedStep).toInt();
@@ -756,7 +747,7 @@ void SettingsWindow::sendSignals()
         emit speedStepAdditive(WIDGET_LOOKUP(ui->playbackSpeedStepAdditive).toBool());
     }
     emit audioFilter("stereotools=balance_out=" +
-        QString().number(WIDGET_LOOKUP(ui->playbackBalance).toDouble()/100), true);
+        QString().number(WIDGET_LOOKUP(ui->audioBalance).toDouble()/100), true);
     emit stepTimeNormal(WIDGET_LOOKUP(ui->playbackNormalStep).toInt());
     emit stepTimeLarge(WIDGET_LOOKUP(ui->playbackLargeStep).toInt());
 
@@ -1104,11 +1095,6 @@ void SettingsWindow::setFreestanding(bool freestanding)
     ui->webLocalFilesBox->setEnabled(yes);
 }
 
-void SettingsWindow::setVolume(int level)
-{
-    WIDGET_LOOKUP(ui->playbackVolume).setValue(level);
-}
-
 void SettingsWindow::setZoomPreset(int which)
 {
     bool autoZoom = which != -1;
@@ -1131,11 +1117,6 @@ void SettingsWindow::setHidePanels(bool hidden)
     emit settingsData(acceptedSettings.toVMap());
 }
 
-void SettingsWindow::self_volumeMax(int maximum)
-{
-    ui->playbackVolume->setMaximum(maximum);
-}
-
 void SettingsWindow::restoreColorControls()
 {
     emit option("brightness", acceptedSettings.value("miscBrightness").value.toInt());
@@ -1147,7 +1128,7 @@ void SettingsWindow::restoreColorControls()
 void SettingsWindow::restoreAudioSettings()
 {
     emit audioFilter("stereotools=balance_out=" +
-        QString().number(WIDGET_LOOKUP(ui->playbackBalance).toDouble()/100), true);
+        QString().number(WIDGET_LOOKUP(ui->audioBalance).toDouble()/100), true);
 }
 
 void SettingsWindow::colorPick_clicked(QLineEdit *colorValue)
@@ -1348,14 +1329,9 @@ void SettingsWindow::on_fullscreenHideControls_toggled(bool checked)
     ui->fullscreenShowWhenDuration->setEnabled(checked);
 }
 
-void SettingsWindow::on_playbackVolume_valueChanged(int value)
+void SettingsWindow::on_audioBalance_valueChanged(int value)
 {
-    QToolTip::showText(QCursor::pos(), QString().number(value), ui->playbackVolume);
-}
-
-void SettingsWindow::on_playbackBalance_valueChanged(int value)
-{
-    QToolTip::showText(QCursor::pos(), QString().number(value), ui->playbackBalance);
+    QToolTip::showText(QCursor::pos(), QString().number(value), ui->audioBalance);
     emit audioFilter("stereotools=balance_out=" + QString().number((double) value/100), true);
 }
 

settingswindow.h
@@ -64,7 +64,6 @@ private:
     void setupPaletteEditor();
     void setupColorPickers();
     void setupFullscreenCombo();
-    void setupSelfSignals();
     void setupUnimplementedWidgets();
     void updateAcceptedSettings();
     SettingMap generateSettingMap(QWidget *root);
@@ -205,12 +204,10 @@ public slots:
     void setServerName(const QString &name);
     void setFreestanding(bool freestanding);
 
-    void setVolume(int level);
     void setZoomPreset(int which);
     void setHidePanels(bool hidden);
 
 private slots:
-    void self_volumeMax(int maximum);
     void restoreColorControls();
     void restoreAudioSettings();
     void colorPick_clicked(QLineEdit *colorValue);
@@ -264,9 +261,7 @@ private slots:
 
     void on_fullscreenHideControls_toggled(bool checked);
 
-    void on_playbackVolume_valueChanged(int value);
-
-    void on_playbackBalance_valueChanged(int value);
+    void on_audioBalance_valueChanged(int value);
 
     void on_playbackAutoZoom_toggled(bool checked);
 

settingswindow.ui
@@ -1337,73 +1337,6 @@ media file played</string>
              </layout>
             </widget>
            </item>
-           <item row="0" column="0" colspan="2">
-            <widget class="QGroupBox" name="playbackAudioBox">
-             <property name="title">
-              <string>Audio</string>
-             </property>
-             <layout class="QGridLayout" name="playbackAudioBoxLayout" columnstretch="1,1">
-              <item row="0" column="0">
-               <widget class="QLabel" name="playbackVolumeLabel">
-                <property name="text">
-                 <string>Volume</string>
-                </property>
-                <property name="alignment">
-                 <set>Qt::AlignmentFlag::AlignCenter</set>
-                </property>
-               </widget>
-              </item>
-              <item row="0" column="1">
-               <widget class="QLabel" name="playbackBalanceLabel">
-                <property name="text">
-                 <string>Balance</string>
-                </property>
-                <property name="alignment">
-                 <set>Qt::AlignmentFlag::AlignCenter</set>
-                </property>
-               </widget>
-              </item>
-              <item row="1" column="1">
-               <widget class="QScrollBar" name="playbackBalance">
-                <property name="cursor">
-                 <cursorShape>PointingHandCursor</cursorShape>
-                </property>
-                <property name="minimum">
-                 <number>-100</number>
-                </property>
-                <property name="maximum">
-                 <number>100</number>
-                </property>
-                <property name="orientation">
-                 <enum>Qt::Orientation::Horizontal</enum>
-                </property>
-               </widget>
-              </item>
-              <item row="1" column="0">
-               <widget class="QSlider" name="playbackVolume">
-                <property name="cursor">
-                 <cursorShape>PointingHandCursor</cursorShape>
-                </property>
-                <property name="maximum">
-                 <number>130</number>
-                </property>
-                <property name="value">
-                 <number>100</number>
-                </property>
-                <property name="orientation">
-                 <enum>Qt::Orientation::Horizontal</enum>
-                </property>
-                <property name="tickPosition">
-                 <enum>QSlider::TickPosition::TicksBothSides</enum>
-                </property>
-                <property name="tickInterval">
-                 <number>10</number>
-                </property>
-               </widget>
-              </item>
-             </layout>
-            </widget>
-           </item>
            <item row="4" column="0">
             <spacer name="playbackSpacer">
              <property name="orientation">
@@ -5687,8 +5620,8 @@ media file played</string>
           </layout>
          </widget>
          <widget class="QWidget" name="audioPage">
-          <layout class="QVBoxLayout" name="audioPageLayout">
-           <item>
+          <layout class="QGridLayout" name="audioPageLayout">
+           <item row="0" column="0" colspan="2">>
             <widget class="QGroupBox" name="audioBox">
              <property name="title">
               <string>Audio Renderer</string>
@@ -6044,7 +5977,7 @@ media file played</string>
              </layout>
             </widget>
            </item>
-           <item>
+           <item row="1" column="0">
             <widget class="QGroupBox" name="audioAutoloadBox">
              <property name="title">
               <string>Audio open settings</string>
@@ -6112,6 +6045,31 @@ media file played</string>
                   <string>All</string>
                  </property>
                 </item>
+               </widget>
+              </item>
+             </layout>
+            </widget>
+           </item>
+           <item row="1" column="1">
+            <widget class="QGroupBox" name="audioBalanceBox">
+             <property name="title">
+              <string>Audio balance</string>
+             </property>
+             <layout class="QGridLayout" name="audioBalanceBoxLayout" columnstretch="1,1">
+              <item colspan="2">>
+               <widget class="QScrollBar" name="audioBalance">
+                <property name="cursor">
+                 <cursorShape>PointingHandCursor</cursorShape>
+                </property>
+                <property name="minimum">
+                 <number>-100</number>
+                </property>
+                <property name="maximum">
+                 <number>100</number>
+                </property>
+                <property name="orientation">
+                 <enum>Qt::Orientation::Horizontal</enum>
+                </property>
                </widget>
               </item>
              </layout>

translations/mpc-qt_ca.ts
@@ -2351,7 +2351,7 @@ arxiu multimèdia reproduït</translation>
     </message>
     <message>
         <source>Volume</source>
-        <translation>Volum</translation>
+        <translation type="vanished">Volum</translation>
     </message>
     <message>
         <source>Use Qt&apos;s inbuilt fusion style</source>
@@ -2359,7 +2359,7 @@ arxiu multimèdia reproduït</translation>
     </message>
     <message>
         <source>Balance</source>
-        <translation>Balanç</translation>
+        <translation type="vanished">Balanç</translation>
     </message>
     <message>
         <source>Default track preference</source>
@@ -4024,6 +4024,10 @@ arxiu multimèdia reproduït</translation>
     </message>
     <message>
         <source>Allows dark theme support on Windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Audio balance</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

translations/mpc-qt_en.ts
@@ -2351,7 +2351,7 @@ media file played</translation>
     </message>
     <message>
         <source>Volume</source>
-        <translation>Volume</translation>
+        <translation type="vanished">Volume</translation>
     </message>
     <message>
         <source>Use Qt&apos;s inbuilt fusion style</source>
@@ -2359,7 +2359,7 @@ media file played</translation>
     </message>
     <message>
         <source>Balance</source>
-        <translation>Balance</translation>
+        <translation type="vanished">Balance</translation>
     </message>
     <message>
         <source>Default track preference</source>
@@ -4025,6 +4025,10 @@ media file played</translation>
     <message>
         <source>Allows dark theme support on Windows</source>
         <translation>Allows dark theme support on Windows</translation>
+    </message>
+    <message>
+        <source>Audio balance</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>

translations/mpc-qt_es.ts
@@ -2323,7 +2323,7 @@ archivo multimedia reproducido</translation>
     </message>
     <message>
         <source>Volume</source>
-        <translation>Volumen</translation>
+        <translation type="vanished">Volumen</translation>
     </message>
     <message>
         <source>Use Qt&apos;s inbuilt fusion style</source>
@@ -2331,7 +2331,7 @@ archivo multimedia reproducido</translation>
     </message>
     <message>
         <source>Balance</source>
-        <translation>Balance</translation>
+        <translation type="vanished">Balance</translation>
     </message>
     <message>
         <source>Default track preference</source>
@@ -3972,6 +3972,10 @@ archivo multimedia reproducido</translation>
     </message>
     <message>
         <source>Allows dark theme support on Windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Audio balance</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

commit f7eab59c563fa3c19ec6f1910e2e48ed024995e0

Don't show "Volume:" OSD message when opening mpc-qt
Fixes regression caused by 7e8ea0289a023d13fa909a4413e72b2dc6a8bb07.
Originally fixed in 1a3f5289528863b51134ff1459dce705d4b5f768.

mainwindow.cpp
@@ -215,8 +215,7 @@ void MainWindow::setState(const QVariantMap &map)
 
     on_actionPlaySubtitlesEnabled_triggered(ui->actionPlaySubtitlesEnabled->isChecked());
     on_actionPlayVolumeMute_toggled(ui->actionPlayVolumeMute->isChecked(), true);
-    volumeSlider_->setValue(map.value("volume", 100).toInt());
-    volume_sliderMoved(volumeSlider_->value());
+    setVolume(map.value("volume", 100).toInt(), true);
     setOSDPage(map.value("shownStatsPage",0).toInt());
     updateOnTop();
 