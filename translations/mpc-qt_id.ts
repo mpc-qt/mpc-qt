3e2201de3af55ab2d46e6fc2b22e63e3825b50c5
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2025-05-07 09:39:09 +0200

    Make percent (%) strings translatable
    Different languages use different spacing or placement of the percent
    sign.
    To allow translation in the QSpinBox widgets, we'll need a custom
    widget, which will probably require translators to retranslate those
    strings.
    
    #410.

--- translations/mpc-qt_id.ts
+++ translations/mpc-qt_id.ts
@@ -1400,6 +1400,38 @@
         <source>&amp;Next Audio Track</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>&amp;50%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;100%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;200%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>25%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;75%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>15&amp;0%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;400%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;300%</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>MouseState</name>
@@ -3976,6 +4008,38 @@ media file played</source>
     </message>
     <message>
         <source>Show video preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>100%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>25%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>50%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>75%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>150%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>200%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>300%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>400%</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
