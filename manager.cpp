mpc-qt/mpc-qt

commit 6f68dc2a70bf1bbfb439458a4497a114e74d4156

Remember video position
Fixes #44
Saves the position in recents and reuses it when opening a file from the
file manager.
Stopping (not pausing) a video resets the position to the start.

manager.cpp
@@ -3,6 +3,7 @@
 #include "mpvwidget.h"
 #include "playlistwindow.h"
 #include "helpers.h"
+#include "logger.h"
 
 using namespace Helpers;
 
@@ -513,15 +514,30 @@ void PlaybackManager::sendCurrentTrackInfo()
                            nowPlayingTitle, mpvLength, mpvTime});
 }
 
+void PlaybackManager::getCurrentTrackInfo(QUrl& url, QUuid& listUuid, QUuid& itemUuid, QString title, double& length, double& position)
+{
+    url = playlistWindow_->getUrlOf(nowPlayingList, nowPlayingItem);
+    listUuid = nowPlayingList;
+    itemUuid = nowPlayingItem;
+    title = nowPlayingTitle;
+    length = mpvLength;
+    position = mpvTime;
+}
+
 void PlaybackManager::startPlayWithUuid(QUrl what, QUuid playlistUuid,
                                         QUuid itemUuid, bool isRepeating,
                                         QUrl with)
 {
+    LogStream("manager") << "startPlayWithUuid";
     if (playbackState_ == WaitingState || what.isEmpty())
         return;
     emit stateChanged(playbackState_ = WaitingState);
 
     mpvStartTime = -1.0;
+
+    if (!isRepeating)
+        emit startingPlayingFile(what);
+
     nowPlaying_ = what;
     mpvObject_->fileOpen(what.isLocalFile() ? what.toLocalFile()
                                             : what.fromPercentEncoding(what.toEncoded()));
@@ -738,6 +754,7 @@ void PlaybackManager::playPrevFile()
 void PlaybackManager::playHalt()
 {
     mpvObject_->stopPlayback();
+    emit stoppedPlaying();
     nowPlaying_.clear();
     nowPlayingItem = QUuid();
     playbackState_ = StoppedState;

manager.h
@@ -73,6 +73,8 @@ signals:
     void systemShouldStandby();
     void systemShouldHibernate();
     void currentTrackInfo(TrackInfo track);
+    void startingPlayingFile(QUrl url);
+    void stoppedPlaying();
 
     void fpsChanged(double fps);
     void avsyncChanged(double sync);
@@ -147,6 +149,7 @@ public slots:
 
     // misc functions
     void sendCurrentTrackInfo();
+    void getCurrentTrackInfo(QUrl &url, QUuid &listUuid, QUuid &itemUuid, QString title, double &length, double &position);
 
 private:
     void startPlayWithUuid(QUrl what, QUuid playlistUuid, QUuid itemUuid,

main.cpp
@@ -749,6 +749,10 @@ void Flow::setupFlowConnections()
     // manager -> this
     connect(playbackManager, &PlaybackManager::nowPlayingChanged,
             this, &Flow::manager_nowPlayingChanged);
+    connect(playbackManager, &PlaybackManager::startingPlayingFile,
+            this, &Flow::manager_startingPlayingFile);
+    connect(playbackManager, &PlaybackManager::stoppedPlaying,
+            this, &Flow::manager_stoppedPlaying);
     connect(playbackManager, &PlaybackManager::stateChanged,
             this, &Flow::manager_stateChanged);
     connect(playbackManager, &PlaybackManager::instanceShouldClose,
@@ -1213,24 +1217,6 @@ void Flow::mainwindow_optionsOpenRequested()
     settingsWindow->raise();
 }
 
-void Flow::manager_nowPlayingChanged(QUrl url, QUuid listUuid, QUuid itemUuid)
-{
-    // Insert playing track as the most recent item
-    TrackInfo track(url, listUuid, itemUuid, QString(), 0, 0);
-    if (recentFiles.contains(track)) {
-        // Remove all prior mention of it
-        recentFiles.removeAll(track);
-    }
-    recentFiles.insert(0, track);
-
-    // Trim the recent file list
-    if (recentFiles.size() > 10)
-        recentFiles.removeLast();
-
-    // Notify (2022-03: the main window) that the recents have changed
-    emit recentFilesChanged(recentFiles);
-}
-
 void Flow::manager_stateChanged(PlaybackManager::PlaybackState state)
 {
     // Do nothing if we don't have to
@@ -1260,6 +1246,26 @@ void Flow::manager_hasNoSubtitles(bool none)
     // Remember that there's no subtitle tracks (used for saving screenshots)
     // or not
     nowPlayingNoSubtitleTracks = none;
+}
+
+void Flow::manager_nowPlayingChanged(QUrl url, QUuid listUuid, QUuid itemUuid) {
+    updateRecentPosition();
+}
+
+void Flow::manager_startingPlayingFile(QUrl url)
+{
+    // Check if there's a position saved in recents for this file
+    foreach (TrackInfo track, recentFiles) {
+        if (track.url == url) {
+            playbackManager->navigateToTime(track.position);
+            break;
+        }
+    }
+}
+void Flow::manager_stoppedPlaying()
+{
+    // Reset the position on stop
+    updateRecentPosition(true);
 }
 
 void Flow::mpcHcServer_fileSelected(QString fileName)
@@ -1373,8 +1379,42 @@ void Flow::favoriteswindow_favoriteTracks(const QList<TrackInfo> &files, const Q
     favoriteStreams = streams;
 }
 
+// Update the position of the current file
+void Flow::updateRecentPosition(bool resetPosition)
+{
+    QUrl url;
+    QUuid listUuid;
+    QUuid itemUuid;
+    QString title;
+    double length;
+    double position;
+    playbackManager->getCurrentTrackInfo(url, listUuid, itemUuid, title, length, position);
+    updateRecents(url, listUuid, itemUuid, title, length, resetPosition ? 0 : position);
+}
+
+// Update the Recents list
+void Flow::updateRecents(QUrl url, QUuid listUuid, QUuid itemUuid, QString title, double length, double position)
+{
+    // Insert playing track as the most recent item
+    TrackInfo track(url, listUuid, itemUuid, title, length, position);
+    if (recentFiles.contains(track)) {
+        // Remove all prior mention of it
+        recentFiles.removeAll(track);
+    }
+    recentFiles.insert(0, track);
+
+    // Trim the recent file list
+    if (recentFiles.size() > 10)
+        recentFiles.removeLast();
+
+    // Notify (2022-03: the main window) that the recents have changed
+    emit recentFilesChanged(recentFiles);
+}
+
 void Flow::endProgram()
 {
+    Logger::log("main", "endProgram");
+    updateRecentPosition();
     // We're about to quit, so write out our config
     writeConfig();
     QMetaObject::invokeMethod(qApp, "exit", Qt::QueuedConnection);

main.h
@@ -50,6 +50,8 @@ private:
     void setupFlowConnections();
     void setupMpris();
     void setupMpcHc();
+    void updateRecentPosition(bool resetPosition = false);
+    void updateRecents(QUrl url, QUuid listUuid, QUuid itemUuid, QString title, double length, double position);
     QByteArray makePayload() const;
     QString pictureTemplate(Helpers::DisabledTrack tracks, Helpers::Subtitles subs) const;
     QVariantList recentToVList() const;
@@ -70,6 +72,8 @@ private slots:
     void manager_stateChanged(PlaybackManager::PlaybackState state);
     void manager_subtitlesVisibile(bool visible);
     void manager_hasNoSubtitles(bool none);
+    void manager_startingPlayingFile(QUrl url);
+    void manager_stoppedPlaying();
     void mpcHcServer_fileSelected(QString fileName);
     void settingswindow_settingsData(const QVariantMap &settings);
     void settingswindow_inhibitScreensaver(bool yes);

commit 9f9cb84394e75d851dfcaa8b1628ec53baf3b391

manager: Send title by reference in getCurrentTrackInfo
Regression introduced by 6f68dc2a70bf1bbfb439458a4497a114e74d4156.
Because of it, the recents file stored the same value for the url and
the title (called "text" in the recent.json file).
This had no visible effect since we didn't use the title until
3f4ec88ad2d7d6b26e6e29ceefed374a25d8ef70.

manager.cpp
@@ -638,7 +638,7 @@ void PlaybackManager::sendCurrentTrackInfo()
                            videoTrackSelected, audioTrackSelected, subtitleTrackSelected});
 }
 
-void PlaybackManager::getCurrentTrackInfo(QUrl& url, QUuid& listUuid, QUuid& itemUuid, QString title,
+void PlaybackManager::getCurrentTrackInfo(QUrl& url, QUuid& listUuid, QUuid& itemUuid, QString& title,
                                           double& length, double& position, int64_t& videoTrack,
                                           int64_t& audioTrack, int64_t& subtitleTrack, bool& hasVideo)
 {

manager.h
@@ -170,7 +170,7 @@ public slots:
 
     // misc functions
     void sendCurrentTrackInfo();
-    void getCurrentTrackInfo(QUrl &url, QUuid &listUuid, QUuid &itemUuid, QString title,
+    void getCurrentTrackInfo(QUrl &url, QUuid &listUuid, QUuid &itemUuid, QString &title,
                              double &length, double &position, int64_t &videoTrack,
                              int64_t &audioTrack, int64_t &subtitleTrack, bool &hasVideo);
 