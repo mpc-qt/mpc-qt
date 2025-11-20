mpc-qt/mpc-qt

commit 5b9df18e10c439524ca1ca63c92e0ea289c67439

settingswindow: Always resend mpv options signals when closing settings
Simplifies the code and allows us to add settings live preview without
having to implement their restoration.

settingswindow.cpp
@@ -1188,20 +1188,6 @@ void SettingsWindow::setCustomMpvOptions()
     }
 }
 
-void SettingsWindow::restoreColorControls()
-{
-    emit option("brightness", WIDGET_LOOKUP(ui->miscBrightness).toInt());
-    emit option("contrast", WIDGET_LOOKUP(ui->miscContrast).toInt());
-    emit option("gamma", WIDGET_LOOKUP(ui->miscGamma).toInt());
-    emit option("hue", WIDGET_LOOKUP(ui->miscHue).toInt());
-    emit option("saturation", WIDGET_LOOKUP(ui->miscSaturation).toInt());
-}
-void SettingsWindow::restoreAudioSettings()
-{
-    setAudioFilter("stereotools", "balance_out=" +
-        QString::number(WIDGET_LOOKUP(ui->audioBalance).toDouble()/100), true);
-}
-
 void SettingsWindow::colorPick_clicked(QLineEdit *colorValue)
 {
     QColor initial = QString("#%1").arg(colorValue->text());
@@ -1261,7 +1247,6 @@ void SettingsWindow::on_buttonBox_clicked(QAbstractButton *button)
         updateAcceptedSettings();
         sendAcceptedSettings();
         actionEditor->updateActions();
-        sendSignals();
     }
     if (buttonRole == QDialogButtonBox::AcceptRole ||
             buttonRole == QDialogButtonBox::RejectRole)
@@ -1271,8 +1256,7 @@ void SettingsWindow::on_buttonBox_clicked(QAbstractButton *button)
 void SettingsWindow::closeEvent(QCloseEvent *event)
 {
     Q_UNUSED(event)
-    restoreColorControls();
-    restoreAudioSettings();
+    sendSignals();
     setCustomMpvOptions();
     ui->keysSearchField->clear();
     ui->videoPreset->setCurrentIndex(0);

settingswindow.h
@@ -221,8 +221,6 @@ public slots:
 private slots:
     void setFilter(QList<QPair<QString, QString>> &filtersList, QString filter, QString options, bool add);
     void setCustomMpvOptions();
-    void restoreColorControls();
-    void restoreAudioSettings();
     void colorPick_clicked(QLineEdit *colorValue);
     void colorPick_changed(const QLineEdit *colorValue, QPushButton *colorPick);
     QList<MpvOption> parseMpvOptions(const QString &optionsInline) const;

commit 040d7a5a51d6bfb6ff6b7c6e270851a672172846

settingswindow: Resend mpv options signals when applying settings
Since the settings window doesn't get closed when applying settings, we
need to resend the mpv options signals so that the new options are
immediately applied.

Fixes regression introduced by 5b9df18e10c439524ca1ca63c92e0ea289c67439.

settingswindow.cpp
@@ -1247,8 +1247,10 @@ void SettingsWindow::on_buttonBox_clicked(QAbstractButton *button)
         sendAcceptedSettings();
         actionEditor->updateActions();
     }
-    if (buttonRole == QDialogButtonBox::ApplyRole)
+    if (buttonRole == QDialogButtonBox::ApplyRole) {
+        sendSignals();
         ui->buttonBox->button(QDialogButtonBox::Apply)->setEnabled(false);
+    }
     else
         close();
 }