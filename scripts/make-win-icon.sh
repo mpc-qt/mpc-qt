77c83eccc39dfec3b7025f8912790ac2e2be81e1
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2025-12-20 17:51:12 +0100

    Use build dir for Windows .ico generated during build

--- scripts/make-win-icon.sh
+++ scripts/make-win-icon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-if [ ! -f src/mpc-qt.ico ]; then
-    magick res/images/icon/mpc-qt.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 -antialias -transparent white -compress zip src/mpc-qt.ico
+ICOFILE="build/mpc-qt.ico"
+mkdir build
+if [ ! -f $ICOFILE ]; then
+    magick res/images/icon/mpc-qt.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 -antialias -transparent white -compress zip $ICOFILE
 fi
-echo mpc-qt.ico
+echo "$ICOFILE written"
