a21e5805fd53e73f3367b6f3396109d55ef93153
Sketch6580 <sketch6580@users.noreply.hosted.weblate.org>
2026-01-27 01:43:34 +0100

    translations: Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 100.0% (1048 of 1048 strings)
    
    Translation: Media Player Classic Qute Theater/mpc-qt-translations
    Translate-URL: https://hosted.weblate.org/projects/mpc-qt/mpc-qt-translations/zh_Hans/

--- translations/mpc-qt_zh_CN.ts
+++ translations/mpc-qt_zh_CN.ts
@@ -1332,7 +1332,7 @@ Do you want to use it for &quot;%3&quot; instead?</source>
     </message>
     <message>
         <source>Re&amp;move File</source>
-        <translation type="unfinished"></translation>
+        <translation>移除文件(&amp;M)</translation>
     </message>
     <message>
         <source>View</source>
@@ -1768,7 +1768,7 @@ Do you want to use it for &quot;%3&quot; instead?</source>
     </message>
     <message>
         <source>&amp;Move File to Recycle Bin</source>
-        <translation type="unfinished"></translation>
+        <translation>将文件移至回收站(&amp;M)</translation>
     </message>
 </context>
 <context>
