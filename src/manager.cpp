mpc-qt/mpc-qt

commit 4483f67559c737cfb5277076618a21184f4a58da

Only use recents selected tracks if they are for the current file
For that, we need to set the tracks after updating nowPlayingItem.
So we save the previous file tracks and position in
Flow::manager_aboutToStartPlayingFile and restore position and tracks
later in Flow::manager_startedPlayingFile.

b2b51c3722e0584f3dfb99506f2b201abf165f50 ("Let selected tracks get
reset when opened from recent files") fixed it only when a file is
opened from recents.
This series of commits fixes it too when a file is opened by different
ways.

src/manager.cpp
@@ -489,24 +489,30 @@ void PlaybackManager::setAudioTrackPreference(QString langs)
 
 void PlaybackManager::setAudioTrack(int64_t id, bool userSelected)
 {
-    if (userSelected)
+    if (userSelected) {
         audioTrackSelected = id;
+        audioTrackSelectedFor = nowPlayingItem;
+    }
     if (id > 0)
         mpvObject_->setAudioTrack(id);
 }
 
 void PlaybackManager::setSubtitleTrack(int64_t id, bool userSelected)
 {
-    if (userSelected)
+    if (userSelected) {
         subtitleTrackSelected = id;
+        subtitleTrackSelectedFor = nowPlayingItem;
+    }
     subtitleTrackActive = id;
     updateSubtitleTrack();
 }
 
 void PlaybackManager::setVideoTrack(int64_t id, bool userSelected)
 {
-    if (userSelected)
+    if (userSelected) {
         videoTrackSelected = id;
+        videoTrackSelectedFor = nowPlayingItem;
+    }
     if (id > 0)
         mpvObject_->setVideoTrack(id);
 }
@@ -690,17 +696,19 @@ void PlaybackManager::startPlayWithUuid(QUrl what, QUuid playlistUuid,
     mpvStartTime = -1.0;
 
     if (!isRepeating)
-        emit startingPlayingFile(what);
+        emit aboutToStartPlayingFile(what);
 
     nowPlaying_ = what;
+    nowPlayingList = playlistUuid;
+    nowPlayingItem = itemUuid;
+    if (!isRepeating)
+        emit startedPlayingFile(what);
     mpvObject_->fileOpen(what.isLocalFile() ? what.toLocalFile()
                                             : QUrl::fromPercentEncoding(what.toEncoded()),
                          replaceMpvPlaylist);
     mpvObject_->setSubFile(with.toString());
     mpvObject_->setPaused(playbackStartPaused);
     playbackStartState = playbackStartPaused ? PausedState : PlayingState;
-    nowPlayingList = playlistUuid;
-    nowPlayingItem = itemUuid;
 
     if (!isRepeating && playbackPlayTimes > 1
             && playlistWindow_->extraPlayTimes(playlistUuid, itemUuid) <= 0) {
@@ -757,9 +765,9 @@ void PlaybackManager::selectDesiredTracks()
                     "lastGoodTrack track auto selected: " + QString::number(lastGoodTrack));
         return lastGoodTrack;
     };
-    int64_t videoId = videoTrackSelected;
-    int64_t audioId = audioTrackSelected;
-    int64_t subsId = subtitleTrackSelected;
+    int64_t videoId = videoTrackSelectedFor == nowPlayingItem ? videoTrackSelected : -1;
+    int64_t audioId = audioTrackSelectedFor == nowPlayingItem ? audioTrackSelected : -1;
+    int64_t subsId = subtitleTrackSelectedFor == nowPlayingItem ? subtitleTrackSelected : -1;
 
     if (audioId < 0)
         audioId = findTrackByLangPreference(audioLangPref, audioListData);

src/manager.h
@@ -91,7 +91,8 @@ signals:
     void systemShouldHibernate();
     void currentTrackInfo(TrackInfo track);
     void openingNewFile();
-    void startingPlayingFile(QUrl url);
+    void aboutToStartPlayingFile(QUrl url);
+    void startedPlayingFile(QUrl url);
     void removePlaylistItemRequested(QUuid itemUuid);
     void stoppedPlaying();
 
@@ -251,8 +252,11 @@ private:
     QStringList audioLangPref;
     QStringList subtitleLangPref;
     int64_t videoTrackSelected = -1;
+    QUuid videoTrackSelectedFor;
     int64_t audioTrackSelected = -1;
+    QUuid audioTrackSelectedFor;
     int64_t subtitleTrackSelected = -1;
+    QUuid subtitleTrackSelectedFor;
     int64_t subtitleTrackActive = 0;
     bool subtitleEnabled = true;
     bool subtitlesIgnoreEmbedded = false;

src/main.cpp
@@ -675,6 +675,10 @@ void Flow::setupManagerConnections()
     connect(playbackManager, &PlaybackManager::currentTrackInfo,
             favoritesWindow, &FavoritesWindow::addTrack);
 
+    // manager -> properties
+    connect(playbackManager, &PlaybackManager::aboutToStartPlayingFile,
+            propertiesWindow, &PropertiesWindow::setFileModifiedTime);
+
     // goto -> manager
     connect(gotoWindow, &GoToWindow::goTo,
             playbackManager, &PlaybackManager::navigateToTime);
@@ -838,8 +842,6 @@ void Flow::setupMpvObjectConnections()
             propertiesWindow, &PropertiesWindow::setMediaLength);
     connect(mpvObject, &MpvObject::videoSizeChanged,
             propertiesWindow, &PropertiesWindow::setVideoSize);
-    connect(playbackManager, &PlaybackManager::startingPlayingFile,
-            propertiesWindow, &PropertiesWindow::setFileModifiedTime);
     connect(mpvObject, &MpvObject::tracksChanged,
             propertiesWindow, &PropertiesWindow::setTracks);
     connect(mpvObject, &MpvObject::mediaTitleChanged,
@@ -906,8 +908,10 @@ void Flow::setupFlowConnections()
             this, &Flow::manager_playLengthChanged);
     connect(playbackManager, &PlaybackManager::openingNewFile,
             this, &Flow::manager_openingNewFile);
-    connect(playbackManager, &PlaybackManager::startingPlayingFile,
-            this, &Flow::manager_startingPlayingFile);
+    connect(playbackManager, &PlaybackManager::aboutToStartPlayingFile,
+            this, &Flow::manager_aboutToStartPlayingFile);
+    connect(playbackManager, &PlaybackManager::startedPlayingFile,
+            this, &Flow::manager_startedPlayingFile);
     connect(playbackManager, &PlaybackManager::stoppedPlaying,
             this, &Flow::manager_stoppedPlaying);
     connect(playbackManager, &PlaybackManager::stateChanged,
@@ -1530,15 +1534,20 @@ void Flow::manager_openingNewFile()
         mainWindow->setActionPlayLoopUse();
 }
 
-void Flow::manager_startingPlayingFile(QUrl url)
+void Flow::manager_aboutToStartPlayingFile()
 {
-    Logger::log(logModule, "manager_startingPlayingFile");
     if (firstFile) {
         firstFile = false;
         mainWindow->fixMpvwSize();
     }
-    if (rememberFilePosition) {
+    if (rememberFilePosition)
         updateRecentPosition(false);
+}
+
+void Flow::manager_startedPlayingFile(QUrl url)
+{
+    Logger::log(logModule, "manager_startedPlayingFile");
+    if (rememberFilePosition) {
         // Check if there's a position saved in recents for this file
         foreach (TrackInfo track, recentFiles) {
             if (track.url == url) {

src/main.h
@@ -86,7 +86,8 @@ private slots:
     void manager_hasNoSubtitles(bool none);
     void manager_playingNextFile();
     void manager_openingNewFile();
-    void manager_startingPlayingFile(QUrl url);
+    void manager_aboutToStartPlayingFile();
+    void manager_startedPlayingFile(QUrl url);
     void manager_stoppedPlaying();
     void mpcHcServer_fileSelected(QString fileName);
     void settingswindow_settingsData(const QVariantMap &settings);

commit 8d90b2edaf27f6edd0dd8933bf677c9222070797

manager: Don't store current tracks selection if auto selected
Don't store current tracks selection in recents if auto selected
(regression introduced by
4483f67559c737cfb5277076618a21184f4a58da).
Also fixes it for favorites.

Fixes an other regression introduced by the same commit but for
next/previous track commands: the auto selected track wasn't the one
used as a starting point.

Fixes (and partially reverts): 4483f67559c737cfb5277076618a21184f4a58da
("Only use recents selected tracks if they are for the current file")

src/manager.cpp
@@ -490,29 +490,26 @@ void PlaybackManager::setAudioTrackPreference(QString langs)
 
 void PlaybackManager::setAudioTrack(int64_t id, bool userSelected)
 {
-    if (userSelected) {
-        audioTrackSelected = id;
-        audioTrackSelectedFor = nowPlayingItem;
-    }
+    audioTrackSelected = id;
+    if (userSelected)
+        audioTrackIsUserSelected = true;
     mpvObject_->setAudioTrack(id);
 }
 
 void PlaybackManager::setSubtitleTrack(int64_t id, bool userSelected)
 {
-    if (userSelected) {
-        subtitleTrackSelected = id;
-        subtitleTrackSelectedFor = nowPlayingItem;
-    }
+    subtitleTrackSelected = id;
+    if (userSelected)
+        subtitleTrackIsUserSelected = true;
     subtitleTrackActive = id;
     updateSubtitleTrack();
 }
 
 void PlaybackManager::setVideoTrack(int64_t id, bool userSelected)
 {
-    if (userSelected) {
-        videoTrackSelected = id;
-        videoTrackSelectedFor = nowPlayingItem;
-    }
+    videoTrackSelected = id;
+    if (userSelected)
+        videoTrackIsUserSelected = true;
     mpvObject_->setVideoTrack(id);
 }
 
@@ -652,7 +649,9 @@ void PlaybackManager::sendCurrentTrackInfo()
     QUrl url(playlistWindow_->getUrlOf(nowPlayingList, nowPlayingItem));
     emit currentTrackInfo({url, nowPlayingList, nowPlayingItem,
                            nowPlayingTitle_, mpvLength, mpvTime,
-                           videoTrackSelected, audioTrackSelected, subtitleTrackSelected});
+                           videoTrackIsUserSelected ? videoTrackSelected : -1,
+                           audioTrackIsUserSelected ? audioTrackSelected : -1,
+                           subtitleTrackIsUserSelected ? subtitleTrackSelected : -1});
 }
 
 void PlaybackManager::getCurrentTrackInfo(QUrl& url, QUuid& listUuid, QUuid& itemUuid, QString& title,
@@ -665,9 +664,9 @@ void PlaybackManager::getCurrentTrackInfo(QUrl& url, QUuid& listUuid, QUuid& ite
     title = nowPlayingTitle_;
     length = mpvLength;
     position = mpvTime;
-    videoTrack = videoTrackSelected;
-    audioTrack = audioTrackSelected;
-    subtitleTrack = subtitleTrackSelected;
+    videoTrack = videoTrackIsUserSelected ? videoTrackSelected : -1;
+    audioTrack = audioTrackIsUserSelected ? audioTrackSelected : -1;
+    subtitleTrack = subtitleTrackIsUserSelected ? subtitleTrackSelected : -1;
     hasVideo = !videoList.isEmpty() && !videoListData[1].isImage;
 }
 
@@ -694,6 +693,12 @@ void PlaybackManager::startPlayWithUuid(QUrl what, QUuid playlistUuid,
     nowPlaying_ = what;
     nowPlayingList = playlistUuid;
     nowPlayingItem = itemUuid;
+    videoTrackSelected = -1;
+    videoTrackIsUserSelected = false;
+    audioTrackSelected = -1;
+    audioTrackIsUserSelected = false;
+    subtitleTrackSelected = -1;
+    subtitleTrackIsUserSelected = false;
     if (!isRepeating)
         emit startedPlayingFile(what);
     mpvObject_->fileOpen(what.isLocalFile() ? what.toLocalFile()
@@ -759,9 +764,9 @@ void PlaybackManager::selectDesiredTracks()
                     "lastGoodTrack track auto selected: " + QString::number(lastGoodTrack));
         return lastGoodTrack;
     };
-    int64_t videoId = videoTrackSelectedFor == nowPlayingItem ? videoTrackSelected : -1;
-    int64_t audioId = audioTrackSelectedFor == nowPlayingItem ? audioTrackSelected : -1;
-    int64_t subsId = subtitleTrackSelectedFor == nowPlayingItem ? subtitleTrackSelected : -1;
+    int64_t videoId = videoTrackSelected;
+    int64_t audioId = audioTrackSelected;
+    int64_t subsId = subtitleTrackSelected;
 
     if (audioId < 0)
         audioId = findTrackByLangPreference(audioLangPref, audioListData);
@@ -785,19 +790,22 @@ void PlaybackManager::updateSubtitleTrack()
 void PlaybackManager::restoreAudioTrack(int64_t id)
 {
     audioTrackSelected = id;
-    audioTrackSelectedFor = nowPlayingItem;
+    if (id > -1)
+        audioTrackIsUserSelected = true;
 }
 
 void PlaybackManager::restoreSubtitleTrack(int64_t id)
 {
     subtitleTrackSelected = id;
-    subtitleTrackSelectedFor = nowPlayingItem;
+    if (id > -1)
+        subtitleTrackIsUserSelected = true;
 }
 
 void PlaybackManager::restoreVideoTrack(int64_t id)
 {
     videoTrackSelected = id;
-    videoTrackSelectedFor = nowPlayingItem;
+    if (id > -1)
+        videoTrackIsUserSelected = true;
 }
 
 void PlaybackManager::checkAfterPlayback()

src/manager.h
@@ -254,11 +254,11 @@ private:
     QStringList audioLangPref;
     QStringList subtitleLangPref;
     int64_t videoTrackSelected = -1;
-    QUuid videoTrackSelectedFor;
+    bool videoTrackIsUserSelected = false;
     int64_t audioTrackSelected = -1;
-    QUuid audioTrackSelectedFor;
+    bool audioTrackIsUserSelected = false;
     int64_t subtitleTrackSelected = -1;
-    QUuid subtitleTrackSelectedFor;
+    bool subtitleTrackIsUserSelected = false;
     int64_t subtitleTrackActive = 0;
     bool subtitleEnabled = true;
     bool subtitlesIgnoreEmbedded = false;