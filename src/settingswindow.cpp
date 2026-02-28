mpc-qt/mpc-qt

commit 11f9a573fb1237cd4c3bb5111a081707d77db19d

Apply language change immediately

src/settingswindow.cpp
@@ -381,6 +381,11 @@ void SettingsWindow::setWaylandOptions(bool isWayland, bool isWaylandMode)
     ui->tweaksPreferWayland->setEnabled(isWayland);
 }
 
+void SettingsWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void SettingsWindow::setupPageTree()
 {
     // Expand every item on pageTree

src/favoriteswindow.cpp
@@ -25,6 +25,11 @@ FavoritesWindow::~FavoritesWindow()
     delete ui;
 }
 
+void FavoritesWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void FavoritesWindow::setFiles(const QList<TrackInfo> &tracks)
 {
     fileList->setTracks(tracks);

src/favoriteswindow.h
@@ -22,6 +22,7 @@ class FavoritesWindow : public QWidget
 public:
     explicit FavoritesWindow(QWidget *parent = nullptr);
     ~FavoritesWindow();
+    void updateLanguage();
 
 signals:
     void favoriteTracks(QList<TrackInfo> files, QList<TrackInfo> streams);

src/gotowindow.cpp
@@ -23,6 +23,11 @@ GoToWindow::~GoToWindow()
     delete ui;
 }
 
+void GoToWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void GoToWindow::init(double currentTime, double maxTime, double fps)
 {
     maxTime_ = maxTime;

src/gotowindow.h
@@ -14,6 +14,7 @@ class GoToWindow : public QWidget
 public:
     explicit GoToWindow(QWidget *parent = nullptr);
     ~GoToWindow();
+    void updateLanguage();
 
 signals:
     void goTo(double time);

src/librarywindow.cpp
@@ -31,6 +31,11 @@ LibraryWindow::~LibraryWindow()
     delete ui;
 }
 
+void LibraryWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void LibraryWindow::refreshLibrary()
 {
     collectionWidget->repopulatePlaylists();

src/librarywindow.h
@@ -20,6 +20,7 @@ class LibraryWindow : public QWidget
 public:
     explicit LibraryWindow(QWidget *parent = nullptr);
     ~LibraryWindow();
+    void updateLanguage();
 
 public slots:
     void refreshLibrary();

src/logwindow.cpp
@@ -29,6 +29,11 @@ LogWindow::~LogWindow()
     delete ui;
 }
 
+void LogWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void LogWindow::appendMessage(QString message)
 {
     ui->messages->appendPlainText(message);

commit 39cf24975f7ffdae94b6c677c48fa195c231aa98

settingswindow: Restore key shortcuts when translating the interface
retranslateUi() resets the key shortcuts from the ui files, so we need
to restore the saved ones.

Fixes: 11f9a573fb1237cd4c3bb5111a081707d77db19d ("Apply language change
immediately")

src/settingswindow.cpp
@@ -384,6 +384,7 @@ void SettingsWindow::setWaylandOptions(bool isWayland, bool isWaylandMode)
 void SettingsWindow::updateLanguage()
 {
     ui->retranslateUi(this);
+    takeKeyMap(acceptedKeyMap);
 }
 
 void SettingsWindow::setupPageTree()
@@ -1270,6 +1271,7 @@ void SettingsWindow::on_buttonBox_clicked(QAbstractButton *button)
     }
     if (buttonRole == QDialogButtonBox::ApplyRole) {
         sendSignals();
+        takeKeyMap(acceptedKeyMap);
         ui->buttonBox->button(QDialogButtonBox::Apply)->setEnabled(false);
     }
     else