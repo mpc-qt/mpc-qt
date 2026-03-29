21d4e2e019341d2f9bfbad1e5dcc7282f75c0dcf
Sketch6580 <sketch6580@users.noreply.hosted.weblate.org>
2026-03-29 06:57:11 +0200

    translations: Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 100.0% (1051 of 1051 strings)
    
    Translation: Media Player Classic Qute Theater/mpc-qt-translations
    Translate-URL: https://hosted.weblate.org/projects/mpc-qt/mpc-qt-translations/zh_Hans/

--- translations/mpc-qt_zh_CN.ts
+++ translations/mpc-qt_zh_CN.ts
@@ -272,7 +272,7 @@ Do you want to use it for &quot;%3&quot; instead?</source>
     </message>
     <message>
         <source>Append the file(s) to the current playlist.</source>
-        <translation type="unfinished"></translation>
+        <translation>将文件添加到当前播放列表。</translation>
     </message>
     <message>
         <source>URLs to open, optionally.</source>
@@ -345,7 +345,7 @@ Do you want to use it for &quot;%3&quot; instead?</source>
     <name>LogWindow</name>
     <message>
         <source>Log Messages</source>
-        <translation>日志</translation>
+        <translation>日志消息</translation>
     </message>
     <message>
         <source>Copy</source>
@@ -353,7 +353,7 @@ Do you want to use it for &quot;%3&quot; instead?</source>
     </message>
     <message>
         <source>Save</source>
-        <translation>粘贴</translation>
+        <translation>保存</translation>
     </message>
     <message>
         <source>Clear</source>
@@ -637,7 +637,7 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>P&amp;roperties</source>
-        <translation>属性(&amp;P)</translation>
+        <translation>属性(&amp;R)</translation>
     </message>
     <message>
         <source>Shift+F10</source>
@@ -1813,15 +1813,15 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>Yes</source>
-        <translation type="unfinished">是</translation>
+        <translation>是</translation>
     </message>
     <message>
         <source>Auto</source>
-        <translation type="unfinished">自动</translation>
+        <translation>自动</translation>
     </message>
     <message>
         <source>No</source>
-        <translation type="unfinished">禁用</translation>
+        <translation>否</translation>
     </message>
 </context>
 <context>
@@ -3026,7 +3026,7 @@ media file played</source>
     </message>
     <message>
         <source>Anti-ring</source>
-        <translation>防边缘伪影‌</translation>
+        <translation>抗振铃</translation>
     </message>
     <message>
         <source>Blur</source>
@@ -3971,7 +3971,7 @@ media file played</source>
     </message>
     <message>
         <source>Drop frames before the seek target in the decoder</source>
-        <translation>跳转时丢弃目标前已解码帧‌</translation>
+        <translation>解码器丢弃跳转目标之前的帧</translation>
     </message>
     <message>
         <source>Show chapter marks in seek bar</source>
