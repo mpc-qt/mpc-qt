mpc-qt/mpc-qt

commit 8d90b2edaf27f6edd0dd8933bf677c9222070797

manager: Don't store current tracks selection if auto selected
Don't store current tracks selection in recents if auto selected
(regression introduced by
4483f67559c737cfb5277076618a21184f4a58da).
Also fixes it for favorites.

Fixes an other regression introduced by the same commit but for
next/previous track commands: the auto selected track wasn't the one
used as a starting point.

Fixes (and partially reverts): 4483f67559c737cfb5277076618a21184f4a58da
("Only use recents selected tracks if they are for the current file")

src/manager.cpp
@@ -490,29 +490,26 @@ void PlaybackManager::setAudioTrackPreference(QString langs)
 
 void PlaybackManager::setAudioTrack(int64_t id, bool userSelected)
 {
-    if (userSelected) {
-        audioTrackSelected = id;
-        audioTrackSelectedFor = nowPlayingItem;
-    }
+    audioTrackSelected = id;
+    if (userSelected)
+        audioTrackIsUserSelected = true;
     mpvObject_->setAudioTrack(id);
 }
 
 void PlaybackManager::setSubtitleTrack(int64_t id, bool userSelected)
 {
-    if (userSelected) {
-        subtitleTrackSelected = id;
-        subtitleTrackSelectedFor = nowPlayingItem;
-    }
+    subtitleTrackSelected = id;
+    if (userSelected)
+        subtitleTrackIsUserSelected = true;
     subtitleTrackActive = id;
     updateSubtitleTrack();
 }
 
 void PlaybackManager::setVideoTrack(int64_t id, bool userSelected)
 {
-    if (userSelected) {
-        videoTrackSelected = id;
-        videoTrackSelectedFor = nowPlayingItem;
-    }
+    videoTrackSelected = id;
+    if (userSelected)
+        videoTrackIsUserSelected = true;
     mpvObject_->setVideoTrack(id);
 }
 
@@ -652,7 +649,9 @@ void PlaybackManager::sendCurrentTrackInfo()
     QUrl url(playlistWindow_->getUrlOf(nowPlayingList, nowPlayingItem));
     emit currentTrackInfo({url, nowPlayingList, nowPlayingItem,
                            nowPlayingTitle_, mpvLength, mpvTime,
-                           videoTrackSelected, audioTrackSelected, subtitleTrackSelected});
+                           videoTrackIsUserSelected ? videoTrackSelected : -1,
+                           audioTrackIsUserSelected ? audioTrackSelected : -1,
+                           subtitleTrackIsUserSelected ? subtitleTrackSelected : -1});
 }
 
 void PlaybackManager::getCurrentTrackInfo(QUrl& url, QUuid& listUuid, QUuid& itemUuid, QString& title,
@@ -665,9 +664,9 @@ void PlaybackManager::getCurrentTrackInfo(QUrl& url, QUuid& listUuid, QUuid& ite
     title = nowPlayingTitle_;
     length = mpvLength;
     position = mpvTime;
-    videoTrack = videoTrackSelected;
-    audioTrack = audioTrackSelected;
-    subtitleTrack = subtitleTrackSelected;
+    videoTrack = videoTrackIsUserSelected ? videoTrackSelected : -1;
+    audioTrack = audioTrackIsUserSelected ? audioTrackSelected : -1;
+    subtitleTrack = subtitleTrackIsUserSelected ? subtitleTrackSelected : -1;
     hasVideo = !videoList.isEmpty() && !videoListData[1].isImage;
 }
 
@@ -694,6 +693,12 @@ void PlaybackManager::startPlayWithUuid(QUrl what, QUuid playlistUuid,
     nowPlaying_ = what;
     nowPlayingList = playlistUuid;
     nowPlayingItem = itemUuid;
+    videoTrackSelected = -1;
+    videoTrackIsUserSelected = false;
+    audioTrackSelected = -1;
+    audioTrackIsUserSelected = false;
+    subtitleTrackSelected = -1;
+    subtitleTrackIsUserSelected = false;
     if (!isRepeating)
         emit startedPlayingFile(what);
     mpvObject_->fileOpen(what.isLocalFile() ? what.toLocalFile()
@@ -759,9 +764,9 @@ void PlaybackManager::selectDesiredTracks()
                     "lastGoodTrack track auto selected: " + QString::number(lastGoodTrack));
         return lastGoodTrack;
     };
-    int64_t videoId = videoTrackSelectedFor == nowPlayingItem ? videoTrackSelected : -1;
-    int64_t audioId = audioTrackSelectedFor == nowPlayingItem ? audioTrackSelected : -1;
-    int64_t subsId = subtitleTrackSelectedFor == nowPlayingItem ? subtitleTrackSelected : -1;
+    int64_t videoId = videoTrackSelected;
+    int64_t audioId = audioTrackSelected;
+    int64_t subsId = subtitleTrackSelected;
 
     if (audioId < 0)
         audioId = findTrackByLangPreference(audioLangPref, audioListData);
@@ -785,19 +790,22 @@ void PlaybackManager::updateSubtitleTrack()
 void PlaybackManager::restoreAudioTrack(int64_t id)
 {
     audioTrackSelected = id;
-    audioTrackSelectedFor = nowPlayingItem;
+    if (id > -1)
+        audioTrackIsUserSelected = true;
 }
 
 void PlaybackManager::restoreSubtitleTrack(int64_t id)
 {
     subtitleTrackSelected = id;
-    subtitleTrackSelectedFor = nowPlayingItem;
+    if (id > -1)
+        subtitleTrackIsUserSelected = true;
 }
 
 void PlaybackManager::restoreVideoTrack(int64_t id)
 {
     videoTrackSelected = id;
-    videoTrackSelectedFor = nowPlayingItem;
+    if (id > -1)
+        videoTrackIsUserSelected = true;
 }
 
 void PlaybackManager::checkAfterPlayback()

src/manager.h
@@ -254,11 +254,11 @@ private:
     QStringList audioLangPref;
     QStringList subtitleLangPref;
     int64_t videoTrackSelected = -1;
-    QUuid videoTrackSelectedFor;
+    bool videoTrackIsUserSelected = false;
     int64_t audioTrackSelected = -1;
-    QUuid audioTrackSelectedFor;
+    bool audioTrackIsUserSelected = false;
     int64_t subtitleTrackSelected = -1;
-    QUuid subtitleTrackSelectedFor;
+    bool subtitleTrackIsUserSelected = false;
     int64_t subtitleTrackActive = 0;
     bool subtitleEnabled = true;
     bool subtitlesIgnoreEmbedded = false;

commit f7af70f90d12e6e873d8b73f5cee72b316abb368

manager: Restart tracks auto selection unless user-selected
When auto selecting tracks, we must not reuse previously auto selected
tracks.
We have to restart the auto selection based on all the available tracks
at the moment unless the tracks have been selected by the user.

This is important because selectDesiredTracks is called more than once
even for the same kind of tracks (e.g. subtitles).

Fixes: 8d90b2edaf27f6edd0dd8933bf677c9222070797 ("manager: Don't store
current tracks selection if auto selected")

Fixes #869 ("Incorrect default audio & subtitle track selection").

src/manager.cpp
@@ -764,8 +764,8 @@ void PlaybackManager::selectDesiredTracks()
                     if (isSubs && it.value().isForced)
                         lastGoodTrack = it.key();
                     else{
-                        Logger::log(logModule,
-                                    "lang track auto selected: " + QString::number(it.key()));
+                        Logger::log(logModule, QString(isSubs ? "subs" : "audio") +
+                                    " lang track auto selected: " + QString::number(it.key()));
                         return it.key();
                     }
                 }
@@ -774,9 +774,13 @@ void PlaybackManager::selectDesiredTracks()
                     "lastGoodTrack track auto selected: " + QString::number(lastGoodTrack));
         return lastGoodTrack;
     };
-    int64_t videoId = videoTrackSelected;
-    int64_t audioId = audioTrackSelected;
-    int64_t subsId = subtitleTrackSelected;
+    int64_t videoId = videoTrackIsUserSelected ? videoTrackSelected : -1;
+    int64_t audioId = audioTrackIsUserSelected ? audioTrackSelected : -1;
+    int64_t subsId = subtitleTrackIsUserSelected ? subtitleTrackSelected : -1;
+
+    Logger::log(logModule, "videoId: " + QString::number(videoId));
+    Logger::log(logModule, "audioId: " + QString::number(audioId));
+    Logger::log(logModule, "subsId: " + QString::number(subsId));
 
     if (audioId < 0)
         audioId = findTrackByLangPreference(audioLangPref, audioListData, false);