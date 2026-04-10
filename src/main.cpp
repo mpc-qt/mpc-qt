mpc-qt/mpc-qt

commit dc7aea2b4ee7fce0a33d84e346ee08bb4a467629

Make Boost optional to build
Fixes #562 (Support for build without boost).

CMakeLists.txt
@@ -33,8 +33,12 @@ endif()
 
 find_package(Qt6 REQUIRED COMPONENTS Gui LinguistTools Network OpenGLWidgets Svg Widgets)
 find_package(PkgConfig REQUIRED)
-find_package(Boost CONFIG REQUIRED)
-message(STATUS "Boost version: ${Boost_VERSION}")
+find_package(Boost CONFIG QUIET)
+if (Boost_FOUND)
+    message(STATUS "Boost: FOUND (${Boost_VERSION})")
+else()
+    message(STATUS "Boost: NOT FOUND")
+endif()
 # These two SET CMAKE_AUTOMOC and CMAKE_AUTOUIC can be replaced with Qt 6.3 by
 # qt_standard_project_setup()
 # Starting with Qt 6.7,
@@ -143,7 +147,9 @@ set(SOURCES
 qt_add_executable(mpc-qt WIN32 MACOSX_BUNDLE ${SOURCES})
 
 target_include_directories(mpc-qt PRIVATE ${CMAKE_BINARY_DIR})
-target_include_directories(mpc-qt PRIVATE ${Boost_INCLUDE_DIRS})
+if (Boost_FOUND)
+    target_include_directories(mpc-qt PRIVATE ${Boost_INCLUDE_DIRS})
+endif()
 
 set(TS_FILES
     translations/mpc-qt_ar.ts
@@ -207,7 +213,9 @@ if(UNIX)
     )
     find_package(Qt6 REQUIRED COMPONENTS DBus)
     target_link_libraries(mpc-qt PRIVATE Qt::DBus)
-    add_compile_definitions(BOOST_STACKTRACE_USE_ADDR2LINE)
+    if (Boost_FOUND)
+        add_compile_definitions(BOOST_STACKTRACE_USE_ADDR2LINE)
+    endif()
 endif()
 
 if(WIN32)

main.cpp
@@ -12,7 +12,9 @@
 #include <QLockFile>
 #include <QThread>
 #include <QTranslator>
+#ifdef Boost_FOUND
 #include <boost/stacktrace.hpp>
+#endif
 #include "logger.h"
 #include "main.h"
 #include "qprocess.h"
@@ -102,9 +104,11 @@ int main(int argc, char *argv[])
 void signalHandler(int signal) {
     if (signal == SIGSEGV || SIGABRT) {
         std::ostringstream stacktrace;
+#ifdef Boost_FOUND
         stacktrace << boost::stacktrace::stacktrace();
         Logger::log("main", "Stack trace:");
         Logger::log("main", QString::fromStdString(stacktrace.str()));
+#endif
         if (signal == SIGSEGV)
             Logger::log("main", "Segmentation fault!");
         Logger::log("main", "Please report this error.");

commit b07fc9d5d863b2dbb9a6a60c05115e93c16abff4

Add preprocessor macro for Boost
Boost_FOUND is a CMake variable, not a preprocessor macro, so Boost was
never used to get a stacktrace since
dc7aea2b4ee7fce0a33d84e346ee08bb4a467629.

Fixes: dc7aea2b4ee7fce0a33d84e346ee08bb4a467629 ("Make Boost optional to
build")

CMakeLists.txt
@@ -117,6 +117,7 @@ add_subdirectory(src)
 target_include_directories(mpc-qt PRIVATE ${CMAKE_BINARY_DIR})
 if (Boost_FOUND)
     target_include_directories(mpc-qt PRIVATE ${Boost_INCLUDE_DIRS})
+    target_compile_definitions(mpc-qt PRIVATE HAVE_BOOST)
 endif()
 
 file(GLOB TS_FILES "${PROJECT_SOURCE_DIR}/translations/*.ts")

src/main.cpp
@@ -11,7 +11,7 @@
 #include <QStyleFactory>
 #include <QLockFile>
 #include <QThread>
-#ifdef Boost_FOUND
+#ifdef HAVE_BOOST
 #include <boost/stacktrace.hpp>
 #endif
 #include "logger.h"
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
 void signalHandler(int signal) {
     if (signal == SIGSEGV || SIGABRT) {
         std::ostringstream stacktrace;
-#ifdef Boost_FOUND
+#ifdef HAVE_BOOST
         stacktrace << boost::stacktrace::stacktrace();
         Logger::log(logModule, "Stack trace:");
         Logger::log(logModule, QString::fromStdString(stacktrace.str()));