mpc-qt/mpc-qt

commit 10f0a1089f147c5f479db46e949db0dcd80d69d8

settingswindow: Use mpv defaults for enabled hardware codecs; add AV1 and VP8
mpv already enables a curated set of hardware codecs by default, so use
these settings by default.

Replace the "All" checkbox by a combobox with "Auto", "All" and
"Custom". "Auto" is selected by default and follows mpv's built-in codec
selection.

Add VP8 and AV1 to the custom codec list, and move HEVC after H.264 to
keep related codecs grouped.

Link: https://mpv.io/manual/master/#options-hwdec-codecs

Mentioned in #884.

src/settingswindow.ui
@@ -5357,14 +5357,29 @@ media file played</string>
               </attribute>
               <layout class="QVBoxLayout" name="verticalLayout_3">
                <item>
-                <widget class="QCheckBox" name="hwdecAll">
-                 <property name="text">
-                  <string>All</string>
-                 </property>
+                <widget class="QComboBox" name="hwdecCodecsMode">
+                 <item>
+                  <property name="text">
+                   <string>Auto</string>
+                  </property>
+                 </item>
+                 <item>
+                  <property name="text">
+                   <string>All</string>
+                  </property>
+                 </item>
+                 <item>
+                  <property name="text">
+                   <string>Custom</string>
+                  </property>
+                 </item>
                 </widget>
                </item>
                <item>
                 <widget class="QWidget" name="hwdecCodecs" native="true">
+                 <property name="enabled">
+                  <bool>false</bool>
+                 </property>
                  <layout class="QVBoxLayout" name="verticalLayout_2">
                   <property name="leftMargin">
                    <number>0</number>
@@ -5433,6 +5448,16 @@ media file played</string>
                    </widget>
                   </item>
                   <item>
+                   <widget class="QCheckBox" name="hwdecHevc">
+                    <property name="text">
+                     <string notr="true">High Efficency Video Coding</string>
+                    </property>
+                    <property name="checked">
+                     <bool>true</bool>
+                    </property>
+                   </widget>
+                  </item>
+                  <item>
                    <widget class="QCheckBox" name="hwdecVc1">
                     <property name="text">
                      <string notr="true">SMPTE VC-1</string>
@@ -5453,9 +5478,9 @@ media file played</string>
                    </widget>
                   </item>
                   <item>
-                   <widget class="QCheckBox" name="hwdecHevc">
+                   <widget class="QCheckBox" name="hwdecVp8">
                     <property name="text">
-                     <string notr="true">High Efficency Video Coding</string>
+                     <string>VP8</string>
                     </property>
                     <property name="checked">
                      <bool>true</bool>
@@ -5465,7 +5490,17 @@ media file played</string>
                   <item>
                    <widget class="QCheckBox" name="hwdecVp9">
                     <property name="text">
-                     <string notr="true">Google VP9</string>
+                     <string notr="true">VP9</string>
+                    </property>
+                    <property name="checked">
+                     <bool>true</bool>
+                    </property>
+                   </widget>
+                  </item>
+                  <item>
+                   <widget class="QCheckBox" name="hwdecAv1">
+                    <property name="text">
+                     <string>AV1</string>
                     </property>
                     <property name="checked">
                      <bool>true</bool>
@@ -8474,22 +8509,6 @@ media file played</string>
     <hint type="destinationlabel">
      <x>258</x>
      <y>60</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>hwdecAll</sender>
-   <signal>toggled(bool)</signal>
-   <receiver>hwdecCodecs</receiver>
-   <slot>setDisabled(bool)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>264</x>
-     <y>72</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>264</x>
-     <y>84</y>
     </hint>
    </hints>
   </connection>

translations/mpc-qt_ar.ts
@@ -4375,6 +4375,14 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ca.ts
@@ -4719,6 +4719,14 @@ arxiu multimèdia reproduït</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_de.ts
@@ -4701,6 +4701,14 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>
@@ -4725,7 +4733,7 @@ media file played</source>
     </message>
     <message>
         <source>%</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%</translation>
     </message>
     <message>
         <source>Width</source>

translations/mpc-qt_en.ts
@@ -4755,6 +4755,14 @@ media file played</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_es.ts
@@ -4559,6 +4559,14 @@ archivo multimedia reproducido</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_fi.ts
@@ -4361,6 +4361,14 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_fr.ts
@@ -4681,6 +4681,14 @@ fichier média lu</translation>
         <source>Use dark colors</source>
         <translation>Utiliser des couleurs sombres</translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

commit acf0a1f86408e19a69c9fa953367e12749cdcd6c

settingswindow: Mark AV1 and VP8 names as not translatable
Fixes: 10f0a1089f147c5f479db46e949db0dcd80d69d8 ("settingswindow: Use
mpv defaults for enabled hardware codecs; add AV1 and VP8")

src/settingswindow.ui
@@ -5480,7 +5480,7 @@ media file played</string>
                   <item>
                    <widget class="QCheckBox" name="hwdecVp8">
                     <property name="text">
-                     <string>VP8</string>
+                     <string notr="true">VP8</string>
                     </property>
                     <property name="checked">
                      <bool>true</bool>
@@ -5500,7 +5500,7 @@ media file played</string>
                   <item>
                    <widget class="QCheckBox" name="hwdecAv1">
                     <property name="text">
-                     <string>AV1</string>
+                     <string notr="true">AV1</string>
                     </property>
                     <property name="checked">
                      <bool>true</bool>

translations/mpc-qt_ar.ts
@@ -4375,14 +4375,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ca.ts
@@ -4719,14 +4719,6 @@ arxiu multimèdia reproduït</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_de.ts
@@ -4701,14 +4701,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_en.ts
@@ -4755,14 +4755,6 @@ media file played</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_es.ts
@@ -4559,14 +4559,6 @@ archivo multimedia reproducido</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_fi.ts
@@ -4361,14 +4361,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_fr.ts
@@ -4683,11 +4683,11 @@ fichier média lu</translation>
     </message>
     <message>
         <source>AV1</source>
-        <translation>AV1</translation>
+        <translation type="vanished">AV1</translation>
     </message>
     <message>
         <source>VP8</source>
-        <translation>VP8</translation>
+        <translation type="vanished">VP8</translation>
     </message>
 </context>
 <context>

translations/mpc-qt_id.ts
@@ -4501,14 +4501,6 @@ file media yang diputar</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_it.ts
@@ -4487,14 +4487,6 @@ ogni file multimediale riprodotto</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ja.ts
@@ -4751,11 +4751,11 @@ media file played</source>
     </message>
     <message>
         <source>AV1</source>
-        <translation>AV1</translation>
+        <translation type="vanished">AV1</translation>
     </message>
     <message>
         <source>VP8</source>
-        <translation>VP8</translation>
+        <translation type="vanished">VP8</translation>
     </message>
 </context>
 <context>

translations/mpc-qt_ko.ts
@@ -4752,14 +4752,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_nb_NO.ts
@@ -4316,14 +4316,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_nl.ts
@@ -4355,14 +4355,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_pl.ts
@@ -4639,14 +4639,6 @@ media file played</source>
         <source>Show video preview (restart required)</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_pt_BR.ts
@@ -4411,14 +4411,6 @@ arquivo de mídia reproduzido</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ru.ts
@@ -4699,14 +4699,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ta.ts
@@ -4729,14 +4729,6 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_tr.ts
@@ -4721,14 +4721,6 @@ yeni bir &amp;oynatıcı aç</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ur.ts
@@ -4503,14 +4503,6 @@ media file played</source>
         <source>Show video preview (restart required)</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>VP8</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>AV1</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_zh_CN.ts
@@ -4627,11 +4627,11 @@ media file played</source>
     </message>
     <message>
         <source>AV1</source>
-        <translation>AV1</translation>
+        <translation type="vanished">AV1</translation>
     </message>
     <message>
         <source>VP8</source>
-        <translation>VP8</translation>
+        <translation type="vanished">VP8</translation>
     </message>
 </context>
 <context>