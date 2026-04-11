mpc-qt/mpc-qt

commit 10f0a1089f147c5f479db46e949db0dcd80d69d8

settingswindow: Use mpv defaults for enabled hardware codecs; add AV1 and VP8
mpv already enables a curated set of hardware codecs by default, so use
these settings by default.

Replace the "All" checkbox by a combobox with "Auto", "All" and
"Custom". "Auto" is selected by default and follows mpv's built-in codec
selection.

Add VP8 and AV1 to the custom codec list, and move HEVC after H.264 to
keep related codecs grouped.

Link: https://mpv.io/manual/master/#options-hwdec-codecs

Mentioned in #884.

src/settingswindow.cpp
@@ -939,9 +939,10 @@ void SettingsWindow::sendSignals()
             backend = "crystalhd";
         emit option("hwdec", backend);
         emit hwdecBackend(backend);
-        if (WIDGET_LOOKUP(ui->hwdecAll).toBool()) {
+        int index = WIDGET_LOOKUP(ui->hwdecCodecsMode).toInt();
+        if (index == 1)
             emit option("hwdec-codecs", "all");
-        } else {
+        else if (index == 2) {
             QStringList codecs;
             if (WIDGET_LOOKUP(ui->hwdecMJpeg).toBool()) codecs << "mjpeg";
             if (WIDGET_LOOKUP(ui->hwdecMpeg1Video).toBool()) codecs << "mpeg1video";
@@ -952,7 +953,9 @@ void SettingsWindow::sendSignals()
             if (WIDGET_LOOKUP(ui->hwdecVc1).toBool()) codecs << "vc1";
             if (WIDGET_LOOKUP(ui->hwdecWmv3).toBool()) codecs << "wmv3";
             if (WIDGET_LOOKUP(ui->hwdecHevc).toBool()) codecs << "hevc";
+            if (WIDGET_LOOKUP(ui->hwdecVp8).toBool()) codecs << "vp8";
             if (WIDGET_LOOKUP(ui->hwdecVp9).toBool()) codecs << "vp9";
+            if (WIDGET_LOOKUP(ui->hwdecAv1).toBool()) codecs << "av1";
             emit option("hwdec-codecs", codecs.join(','));
         }
     } else {
@@ -1587,6 +1590,17 @@ void SettingsWindow::on_ditherTemporal_toggled(bool checked)
 {
     ui->ditherTemporalPeriod->setEnabled(checked);
     ui->ditherTemporalPeriodLabel->setEnabled(checked);
+}
+
+void SettingsWindow::on_hwdecCodecsMode_currentIndexChanged(int index)
+{
+    ui->hwdecCodecs->setEnabled(index == 2);
+}
+
+void SettingsWindow::on_hwdecEnable_toggled(bool checked)
+{
+    if (checked)
+        on_hwdecCodecsMode_currentIndexChanged(ui->hwdecCodecsMode->currentIndex());
 }
 
 void SettingsWindow::on_audioSpdif_toggled(bool checked)

src/settingswindow.h
@@ -311,6 +311,10 @@ private slots:
 
     void on_ditherTemporal_toggled(bool checked);
 
+    void on_hwdecCodecsMode_currentIndexChanged(int index);
+
+    void on_hwdecEnable_toggled(bool checked);
+
     void on_audioSpdif_toggled(bool checked);
 
     void on_subsBackgroundBoxEnabled_toggled(bool checked);

src/settingswindow.ui
@@ -5357,14 +5357,29 @@ media file played</string>
               </attribute>
               <layout class="QVBoxLayout" name="verticalLayout_3">
                <item>
-                <widget class="QCheckBox" name="hwdecAll">
-                 <property name="text">
-                  <string>All</string>
-                 </property>
+                <widget class="QComboBox" name="hwdecCodecsMode">
+                 <item>
+                  <property name="text">
+                   <string>Auto</string>
+                  </property>
+                 </item>
+                 <item>
+                  <property name="text">
+                   <string>All</string>
+                  </property>
+                 </item>
+                 <item>
+                  <property name="text">
+                   <string>Custom</string>
+                  </property>
+                 </item>
                 </widget>
                </item>
                <item>
                 <widget class="QWidget" name="hwdecCodecs" native="true">
+                 <property name="enabled">
+                  <bool>false</bool>
+                 </property>
                  <layout class="QVBoxLayout" name="verticalLayout_2">
                   <property name="leftMargin">
                    <number>0</number>
@@ -5433,6 +5448,16 @@ media file played</string>
                    </widget>
                   </item>
                   <item>
+                   <widget class="QCheckBox" name="hwdecHevc">
+                    <property name="text">
+                     <string notr="true">High Efficency Video Coding</string>
+                    </property>
+                    <property name="checked">
+                     <bool>true</bool>
+                    </property>
+                   </widget>
+                  </item>
+                  <item>
                    <widget class="QCheckBox" name="hwdecVc1">
                     <property name="text">
                      <string notr="true">SMPTE VC-1</string>
@@ -5453,9 +5478,9 @@ media file played</string>
                    </widget>
                   </item>
                   <item>
-                   <widget class="QCheckBox" name="hwdecHevc">
+                   <widget class="QCheckBox" name="hwdecVp8">
                     <property name="text">
-                     <string notr="true">High Efficency Video Coding</string>
+                     <string>VP8</string>
                     </property>
                     <property name="checked">
                      <bool>true</bool>
@@ -5465,7 +5490,17 @@ media file played</string>
                   <item>
                    <widget class="QCheckBox" name="hwdecVp9">
                     <property name="text">
-                     <string notr="true">Google VP9</string>
+                     <string notr="true">VP9</string>
+                    </property>
+                    <property name="checked">
+                     <bool>true</bool>
+                    </property>
+                   </widget>
+                  </item>
+                  <item>
+                   <widget class="QCheckBox" name="hwdecAv1">
+                    <property name="text">
+                     <string>AV1</string>
                     </property>
                     <property name="checked">
                      <bool>true</bool>
@@ -8474,22 +8509,6 @@ media file played</string>
     <hint type="destinationlabel">
      <x>258</x>
      <y>60</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>hwdecAll</sender>
-   <signal>toggled(bool)</signal>
-   <receiver>hwdecCodecs</receiver>
-   <slot>setDisabled(bool)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>264</x>
-     <y>72</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>264</x>
-     <y>84</y>
     </hint>
    </hints>
   </connection>

translations/mpc-qt_ar.ts
@@ -4375,6 +4375,14 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ca.ts
@@ -4719,6 +4719,14 @@ arxiu multimèdia reproduït</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_de.ts
@@ -4701,6 +4701,14 @@ media file played</source>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>
@@ -4725,7 +4733,7 @@ media file played</source>
     </message>
     <message>
         <source>%</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%</translation>
     </message>
     <message>
         <source>Width</source>

translations/mpc-qt_en.ts
@@ -4755,6 +4755,14 @@ media file played</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_es.ts
@@ -4559,6 +4559,14 @@ archivo multimedia reproducido</translation>
         <source>Use dark colors</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AV1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VP8</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

commit 9451736d0b2dc73425e53d24451973d46db7d728

settingswindow: Don't empty hwdec-codecs when disabling hardware decoding
Hardware decoding wasn't being re-enabled until restart after being
disabled then re-enabled by the user because hwdec-codecs was staying
empty.

There doesn't seem to be a way to restore defaults for hwdec-codecs once
we change it.

We could set it to the current default codecs of the latest version of
mpv, but then we'd need to update it every time it changes.

The only case where it won't use the defaults until restart is if the
user enables All or Custom codecs then changes back to Auto.

Fixes: 10f0a1089f147c5f479db46e949db0dcd80d69d8 ("settingswindow: Use
mpv defaults for enabled hardware codecs; add AV1 and VP8")

src/settingswindow.cpp
@@ -960,7 +960,6 @@ void SettingsWindow::sendSignals()
         }
     } else {
         emit option("hwdec", "no");
-        emit option("hwdec-codecs", "");
         emit hwdecBackend("no");
     }
 