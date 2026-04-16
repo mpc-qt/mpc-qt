mpc-qt/mpc-qt

commit e384c907db5a756d832f38278d64aa361ecedc3b

Save last window size before it got maximized or made fullscreen
Qt only reports the window as maximized after it got resized to its
maximized size.
So we have to restore the last known size once that
event gets triggered. Otherwise, the saved size (used when the window is
restored down) would be the same as the maximized size.

src/main.cpp
@@ -910,6 +910,10 @@ void Flow::setupFlowConnections()
             this, &Flow::mainwindow_fullscreenHideControls);
     connect(mainWindow, &MainWindow::repeatAfter,
             this, &Flow::mainwindow_repeatAfter);
+    connect(mainWindow, &MainWindow::windowResized,
+            this, &Flow::mainwindow_windowResized);
+    connect(mainWindow, &MainWindow::windowMaximized,
+            this, &Flow::mainwindow_windowMaximized);
 
     // manager -> this
     connect(playbackManager, &PlaybackManager::playLengthChanged,
@@ -1386,6 +1390,16 @@ void Flow::mainwindow_fullscreenHideControls(bool hide)
 void Flow::mainwindow_repeatAfter()
 {
     repeatAfter = true;
+}
+
+void Flow::mainwindow_windowResized()
+{
+    windowManager.updateAppWindowGeometryCache(mainWindow, false);
+}
+
+void Flow::mainwindow_windowMaximized()
+{
+    windowManager.updateAppWindowGeometryCache(mainWindow, true);
 }
 
 void Flow::mainwindow_recentOpened(const TrackInfo &track)

src/main.h
@@ -74,6 +74,8 @@ private slots:
     void mainwindow_instanceShouldQuit();
     void mainwindow_fullscreenHideControls(bool hide);
     void mainwindow_repeatAfter();
+    void mainwindow_windowResized();
+    void mainwindow_windowMaximized();
     void mainwindow_recentOpened(const TrackInfo &track);
     void mainwindow_recentClear();
     void mainwindow_takeImage(Helpers::ScreenshotRender render);

src/mainwindow.cpp
@@ -379,6 +379,7 @@ void MainWindow::resizeEvent(QResizeEvent *event)
     updateBottomAreaGeometry();
     checkBottomArea(QCursor::pos());
     resizePlaylistToFit();
+    emit windowResized();
 }
 
 void MainWindow::changeEvent(QEvent *event)
@@ -394,6 +395,8 @@ void MainWindow::changeEvent(QEvent *event)
         if (videoPreview)
             videoPreview->updatePalette();
     }
+    else if (event->type() == QEvent::WindowStateChange && isMaximized())
+        emit windowMaximized();
 }
 
 bool MainWindow::eventFilter(QObject *object, QEvent *event)

src/mainwindow.h
@@ -173,6 +173,8 @@ signals:
     void chapterSelected(int64_t id);
     void timeSelected(double time);
     void fullscreenModeChanged(bool fullscreen);
+    void windowResized();
+    void windowMaximized();
     void zoomPresetChanged(int which);
     void playCurrentItemRequested();
     void favoriteCurrentTrack();

src/platform/windowmanager.cpp
@@ -4,6 +4,7 @@
 #include <QStyle>
 #include <QWidget>
 #include "helpers.h"
+#include "logger.h"
 #include "mainwindow.h"
 #include "windowmanager.h"
 
@@ -36,19 +37,26 @@ QVariantMap WindowManager::json()
 
 void WindowManager::saveAppWindow(MainWindow *window, bool rememberWindowGeometry, bool rememberPanels)
 {
-    QVariantMap geometry;
     QVariantMap panels;
-    if (rememberWindowGeometry)
-        geometry = Helpers::rectToVmap(QRect(window->geometry().topLeft(),
-                                            window->size()));
     if (rememberPanels)
         panels = window->state();
     QVariantMap data = {
-        { keyGeometry, geometry },
+        { keyGeometry, rememberWindowGeometry ? appWindowGeometryCurrent : QVariantMap() },
         { keyState, panels },
         { keyMaximized, rememberWindowGeometry ? window->isMaximized() : false }
     };
     json_.insert(window->objectName(), data);
+}
+
+void WindowManager::updateAppWindowGeometryCache(const MainWindow *window, bool restorePrevious)
+{
+    if (restorePrevious || window->isMaximized() || window->isFullScreen())
+        appWindowGeometryCurrent = appWindowGeometryPrevious;
+    else {
+        appWindowGeometryPrevious = appWindowGeometryCurrent;
+        appWindowGeometryCurrent = Helpers::rectToVmap(QRect(window->geometry().topLeft(),
+                                            window->size()));
+    }
 }
 
 void WindowManager::saveDocks(QMainWindow *dockHost)
@@ -73,6 +81,8 @@ void WindowManager::restoreAppWindow(MainWindow *window, const CliInfo &cliInfo)
 
     // restore main window geometry and override it if requested
     QRect geometry = Helpers::vmapToRect(data[keyGeometry].toMap());
+    appWindowGeometryCurrent = data[keyGeometry].toMap();
+    appWindowGeometryPrevious = appWindowGeometryCurrent;
     QPoint desiredPlace = geometry.topLeft();
     QSize desiredSize = geometry.size();
     bool checkMainWindow = data.isEmpty() || geometry.isEmpty();

src/platform/windowmanager.h
@@ -35,6 +35,7 @@ public:
     QVariantMap json();
 
     void saveAppWindow(MainWindow *window, bool rememberWindowGeometry, bool rememberPanels);
+    void updateAppWindowGeometryCache(const MainWindow *window, bool restorePrevious);
     void saveDocks(QMainWindow *dockHost);
     void saveWindow(QWidget *window);
 
@@ -49,6 +50,8 @@ signals:
 
 private:
     QVariantMap json_;
+    QVariantMap appWindowGeometryCurrent;
+    QVariantMap appWindowGeometryPrevious;
 };
 
 #endif // WINDOWMANAGER_H

commit 2227452aa15112250b28ceb49e71593bd6cf20d0

Update geometry cache on window move to save position
Since e384c907db5a756d832f38278d64aa361ecedc3b the window geometry
(which includes its position) isn't checked directly on save.
Instead, its cache is used, so that cache has to be updated on each
window move as well.

Tested with XWayland, with X11 (Flatpak version) and on Windows 10.

Fixes: e384c907db5a756d832f38278d64aa361ecedc3b ("Save last window size
before it got maximized or made fullscreen")

src/main.cpp
@@ -937,6 +937,8 @@ void Flow::setupFlowConnections()
             this, &Flow::mainwindow_windowResized);
     connect(mainWindow, &MainWindow::windowMaximized,
             this, &Flow::mainwindow_windowMaximized);
+    connect(mainWindow, &MainWindow::windowMoved,
+            this, &Flow::mainwindow_windowMoved);
 
     // manager -> this
     connect(playbackManager, &PlaybackManager::playLengthChanged,
@@ -1424,6 +1426,11 @@ void Flow::mainwindow_windowResized()
 void Flow::mainwindow_windowMaximized()
 {
     windowManager.updateAppWindowGeometryCache(mainWindow, true);
+}
+
+void Flow::mainwindow_windowMoved()
+{
+    windowManager.updateAppWindowGeometryCache(mainWindow, false);
 }
 
 void Flow::mainwindow_recentOpened(const TrackInfo &track)

src/main.h
@@ -76,6 +76,7 @@ private slots:
     void mainwindow_repeatAfter();
     void mainwindow_windowResized();
     void mainwindow_windowMaximized();
+    void mainwindow_windowMoved();
     void mainwindow_recentOpened(const TrackInfo &track);
     void mainwindow_recentClear();
     void mainwindow_takeImage(Helpers::ScreenshotRender render);

src/mainwindow.cpp
@@ -399,6 +399,12 @@ void MainWindow::changeEvent(QEvent *event)
         emit windowMaximized();
 }
 
+void MainWindow::moveEvent(QMoveEvent *event)
+{
+    Q_UNUSED(event)
+    emit windowMoved();
+}
+
 bool MainWindow::eventFilter(QObject *object, QEvent *event)
 {
     bool insideMpv = mpvw ? object == mpvw : false;

src/mainwindow.h
@@ -55,6 +55,7 @@ public:
 protected:
     void resizeEvent(QResizeEvent *event);
     void changeEvent(QEvent *event) override;
+    void moveEvent(QMoveEvent *event) override;
     bool eventFilter(QObject *object, QEvent *event);
     void closeEvent(QCloseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);
@@ -177,6 +178,7 @@ signals:
     void fullscreenModeChanged(bool fullscreen);
     void windowResized();
     void windowMaximized();
+    void windowMoved();
     void zoomPresetChanged(int which);
     void playCurrentItemRequested();
     void favoriteCurrentTrack();