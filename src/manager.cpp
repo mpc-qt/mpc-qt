mpc-qt/mpc-qt

commit 5dc40a204790cae93de36c2f32b867b0ea445f32

Make "Previous" seek to beginning of current chapter if not beyond 5 seconds
When using the "seek to previous chapter" command, both MPC-HC and mpv
have the following behavior:
- if more than 5 seconds have passed since the chapter start, seek
to the beginning of the current chapter
- otherwise, seek to the previous chapter.

Since our "Previous" command can also open the previous
file, we can't just use the mpv command.

We could check the "chapter-seek-threshold" property instead of using
the default mpv value. But it would make more sense to have an
"advanced" option that, though MPC-HC doesn't seem to have one.

Fixes #855 ("In a MKV with chapters, Prev does not behave as in mpc-hc
or desirably").

src/manager.cpp
@@ -328,9 +328,8 @@ void PlaybackManager::navigateToNextChapter()
 
 void PlaybackManager::navigateToPrevChapter()
 {
-    int64_t chapter = mpvObject_->chapter();
-    if (chapter > 0)
-        navigateToChapter(std::max(int64_t(0), chapter - 1));
+    if (mpvTime > 5)
+        changeChapter(-1);
     else
         playPrev(false);
 }
@@ -400,6 +399,11 @@ void PlaybackManager::repeatThisFile()
 void PlaybackManager::deltaExtraPlaytimes(int delta)
 {
     playlistWindow_->deltaExtraPlayTimes(nowPlayingList, nowPlayingItem, delta);
+}
+
+void PlaybackManager::changeChapter(int64_t diff)
+{
+    mpvObject_->changeChapter(diff);
 }
 
 void PlaybackManager::navigateToChapter(int64_t chapter)

src/manager.h
@@ -134,6 +134,7 @@ public slots:
     void closingPlaylist(QUuid playlist);
     void repeatThisFile();
     void deltaExtraPlaytimes(int delta);
+    void changeChapter(int64_t diff);
     void navigateToChapter(int64_t chapter);
     void navigateToTime(double time);
     void speedUp();

src/mpvwidget.cpp
@@ -537,6 +537,11 @@ int64_t MpvObject::chapter()
     return chapter_;
 }
 
+void MpvObject::changeChapter(int diff)
+{
+    emit ctrlCommand(QStringList({"add", "chapter", QString::number(diff)}));
+}
+
 bool MpvObject::setChapter(int64_t chapter)
 {
     // As this requires knowledge of mpv's return value, it cannot be

src/mpvwidget.h
@@ -86,6 +86,7 @@ public:
     void setVideoFlip(bool flip);
 
     int64_t chapter();
+    void changeChapter(int diff);
     bool setChapter(int64_t chapter);
     QString mediaTitle();
     void setMute(bool yes);

commit 46214a7b51cd728cae6c453a3a6f86251930e0b8

manager: Make "Previous" seek to start for files without chapters
When on the first chapter and past 5 seconds, "Previous" now seeks to
the beginning of the file instead of attempting a chapter change.
This aligns the behavior between files with and without chapters, and
restores correct "previous track/file" handling when past 5 seconds in
files that lack chapter metadata.

The mpv chapter command is a no-op when no chapters are present, which
previously caused the action to fail.

Fixes: 5dc40a204790cae93de36c2f32b867b0ea445f32 ('Make 'Previous' seek
to beginning of current chapter if not beyond 5 seconds')
Fixes #924 ('Previous file" function with mouse button breaks after you
jump on the seekbar').

src/manager.cpp
@@ -333,8 +333,12 @@ void PlaybackManager::navigateToNextChapter()
 
 void PlaybackManager::navigateToPrevChapter()
 {
-    if (mpvTime > 5)
-        changeChapter(-1);
+    if (mpvTime > 5) {
+        if (mpvObject_->chapter() > 0)
+            changeChapter(-1);
+        else
+            navigateToTime(0);
+    }
     else
         playPrev(false);
 }