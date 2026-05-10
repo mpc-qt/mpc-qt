mpc-qt/mpc-qt

commit 0223400f8c6e2b4e2a47600066066af01c4aa9e2

windowmanager: Delay setting geometry when exiting fullscreen
Fixes two problems when exiting fullscreen in Wayland mode:
- visual glitches
- with the KDE Plasma fullscreen "stretch" effect, the window was shown
gliding from the top left corner to its original position.

Tested on Windows 10, Mint Cinnamon and Fedora GNOME with no side
effects.

src/platform/windowmanager.cpp
@@ -161,7 +161,9 @@ FullscreenMemory WindowManager::makeFullscreen(QWidget *who, QString preferredSc
 void WindowManager::restoreFullscreen(QWidget *who, const FullscreenMemory &fm)
 {
     who->showNormal();
-    who->setGeometry(fm.normalGeometry);
+    QMetaObject::invokeMethod(who, [who, fm]() {
+        who->setGeometry(fm.normalGeometry);
+    }, Qt::SingleShotConnection);
 
     QScreen *target = Helpers::findScreenByName(fm.originalScreen);
     if (target) {

commit 05f5f83d17967629b98342970f5514b3cf9169d5

windowmanager: Don't set geometry when exiting fullscreen in Wayland mode
Qt::SingleShotConnection isn't enough for QMetaObject::invokeMethod, so
the code wasn't run anymore.
Since it did fix it in Wayland mode, just disable it in that case.

Link: https://doc.qt.io/qt-6/qt.html#ConnectionType-enum

Fixes: 0223400f8c6e2b4e2a47600066066af01c4aa9e2 ("windowmanager: Delay
setting geometry when exiting fullscreen")

src/platform/windowmanager.cpp
@@ -161,9 +161,8 @@ FullscreenMemory WindowManager::makeFullscreen(QWidget *who, QString preferredSc
 void WindowManager::restoreFullscreen(QWidget *who, const FullscreenMemory &fm)
 {
     who->showNormal();
-    QMetaObject::invokeMethod(who, [who, fm]() {
+    if (QGuiApplication::platformName() != "wayland")
         who->setGeometry(fm.normalGeometry);
-    }, Qt::SingleShotConnection);
 
     QScreen *target = Helpers::findScreenByName(fm.originalScreen);
     if (target) {