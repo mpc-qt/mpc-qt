58063b2cb1a6905b832d91e4ba27a4ac8b65f309
coolvitto <coolzdaddy55@gmail.com>
2026-05-15 18:55:40 +0200

    translations: Translated using Weblate (Japanese)
    Currently translated at 100.0% (1064 of 1064 strings)
    
    Translation: Media Player Classic Qute Theater/mpc-qt-translations
    Translate-URL: https://hosted.weblate.org/projects/mpc-qt/mpc-qt-translations/ja/

--- translations/mpc-qt_ja.ts
+++ translations/mpc-qt_ja.ts
@@ -893,7 +893,7 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>&amp;Up</source>
-        <translation>音量を上げる(&amp;U)</translation>
+        <translation>上(&amp;U)</translation>
     </message>
     <message>
         <source>0</source>
@@ -901,7 +901,7 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>&amp;Down</source>
-        <translation>音量を下げる(&amp;D)</translation>
+        <translation>下(&amp;D)</translation>
     </message>
     <message>
         <source>9</source>
@@ -1189,7 +1189,7 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>&amp;Increment</source>
-        <translation>拡大(&amp;I)</translation>
+        <translation>上げる(&amp;I)</translation>
     </message>
     <message>
         <source>E</source>
@@ -1197,7 +1197,7 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>&amp;Decrement</source>
-        <translation>縮小(&amp;D)</translation>
+        <translation>下げる(&amp;D)</translation>
     </message>
     <message>
         <source>Shift+E</source>
@@ -4818,11 +4818,11 @@ media file played</source>
     </message>
     <message>
         <source>Played</source>
-        <translation type="unfinished"></translation>
+        <translation>再生</translation>
     </message>
     <message>
         <source>Remaining</source>
-        <translation type="unfinished"></translation>
+        <translation>残り時間</translation>
     </message>
 </context>
 <context>
