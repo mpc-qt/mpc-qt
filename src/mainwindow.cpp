mpc-qt/mpc-qt

commit dffbc6bb1223920ba8c03acd0d9011c57e3de8e5

Simplify the way the title bar is set
There were two sources of the same data, with only one of them being
actually used. So change it to:
PropertiesWindow sends the title in "artist - title" format for audio
files.
PlaybackManager sends the title when it changes. Reuse the file name
already set for the video preview.
File name and full path are formatted in MainWindow::setMediaTitle.

Fixes special characters being URL encoded for full path mode.

Fixes #952 ("URL encoding used in title bar if unusual character in path
or filename").

src/mainwindow.cpp
@@ -1788,7 +1788,7 @@ void MainWindow::httpAfterPlaybackLock()
 void MainWindow::setFreestanding(bool freestanding)
 {
     freestanding_ = freestanding;
-    setMediaTitleWithFilename(QString(), QString());
+    setMediaTitle(QString());
 }
 
 void MainWindow::setNoVideoSize(const QSize &size)
@@ -1808,7 +1808,7 @@ void MainWindow::setTrayIcon(bool enabled)
 void MainWindow::setTitleBarFormat(Helpers::TitlePrefix titlebarFormat)
 {
     titlebarFormat_ = titlebarFormat;
-    setMediaTitleWithFilename(currentFileTitle, currentFileName);
+    setMediaTitle(currentFileTitle);
 }
 
 void MainWindow::setWindowedMouseMap(const MouseStateMap &map)
@@ -1955,20 +1955,20 @@ void MainWindow::setTime(double time, double length)
     statusTime->setTime(time, length);
 }
 
-void MainWindow::setMediaTitleWithFilename(const QString& title, const QString& filename)
+void MainWindow::setMediaTitle(const QString &title)
 {
     currentFileTitle = title;
-    currentFileName = filename;
     QString newTitle = title;
     QString windowTitle(textWindowTitle);
 
     switch(titlebarFormat_) {
         case PrefixFullPath:
-            newTitle = currentFile.toString();
+            if (currentFile.isLocalFile())
+                newTitle = currentFile.path();
             break;
         case PrefixFileName:
-            if (!filename.isEmpty())
-                newTitle = filename;
+            if (currentFile.isLocalFile())
+                newTitle = currentFile.fileName();
             break;
         case PrefixFileTitle:
             break;

src/main.cpp
@@ -605,7 +605,7 @@ void Flow::setupMainWindowConnections()
 
     // propertieswindow -> mainwindow
     connect(propertiesWindow, &PropertiesWindow::artistAndTitleChanged,
-            mainWindow, &MainWindow::setMediaTitleWithFilename);
+            mainWindow, &MainWindow::setMediaTitle);
 
     // mainwindow -> playlistwindow
     connect(mainWindow, &MainWindow::playCurrentItemRequested,
@@ -618,8 +618,8 @@ void Flow::setupMainWindowConnections()
     // manager -> mainwindow
     connect(playbackManager, &PlaybackManager::timeChanged,
             mainWindow, &MainWindow::setTime);
-    connect(playbackManager, &PlaybackManager::titleChangedWithFilename,
-            mainWindow, &MainWindow::setMediaTitleWithFilename);
+    connect(playbackManager, &PlaybackManager::titleChanged,
+            mainWindow, &MainWindow::setMediaTitle);
     connect(playbackManager, &PlaybackManager::videoSizeChanged,
             mainWindow, &MainWindow::setVideoSize);
     connect(playbackManager, &PlaybackManager::stateChanged,

src/mainwindow.h
@@ -259,7 +259,7 @@ public slots:
     void setHighContrastWidgets(bool yes);
     void setInfoColors(const QColor &foreground, const QColor &background);
     void setTime(double time, double length);
-    void setMediaTitleWithFilename(const QString& title, const QString& filename);
+    void setMediaTitle(const QString &title);
     void setChapterTitle(QString title);
     void setAspectName(QString aspectName);
     void setVideoSize(QSize size);
@@ -578,7 +578,6 @@ private:
     int currentAngle = 0;
     bool flipped = false;
     QUrl currentFile;
-    QString currentFileName;
     QString currentFileTitle;
 
     IconThemer themer;

src/manager.cpp
@@ -1096,8 +1096,6 @@ void PlaybackManager::mpvw_mediaTitleChanged(QString title)
 {
     nowPlayingTitle_ = title;
     emit titleChanged(title);
-    emit titleChangedWithFilename(title,
-                                  nowPlaying().isLocalFile() ? nowPlaying().fileName() : QString());
 }
 
 void PlaybackManager::mpvw_chaptersChanged(QVariantList chapters)

src/manager.h
@@ -63,7 +63,6 @@ signals:
     void timeChanged(double time, double length);
     void playLengthChanged();
     void titleChanged(QString title);
-    void titleChangedWithFilename(QString title, QString filename);
     void videoSizeChanged(QSize size);
     void playbackSpeedChanged(double speed);
     void stateChanged(PlaybackManager::PlaybackState state, int bufferFillState = 0);

src/propertieswindow.cpp
@@ -195,8 +195,7 @@ void PropertiesWindow::setMetaData(QVariantMap data)
 
     generalData.insert(data);
     if (data.contains("artist") && data.contains("title"))
-        emit artistAndTitleChanged(data["artist"].toString() + " - " + data["title"].toString(),
-                                   this->filename);
+        emit artistAndTitleChanged(data["artist"].toString() + " - " + data["title"].toString());
     updateLastTab();
 }
 

src/propertieswindow.h
@@ -20,7 +20,7 @@ public:
     void updateLanguage();
 
 signals:
-    void artistAndTitleChanged(QString artistAndTitle, QString filename);
+    void artistAndTitleChanged(QString artistAndTitle);
 
 public slots:
     void setFileName(const QString &filename);

commit c08cd1aa778024c5437f7201e395a3ab429fad7f

mainwindow: Empty title bar on stop
The file name isn't sent anymore from the PlaybackManager since
dffbc6bb1223920ba8c03acd0d9011c57e3de8e5 so the title bar wouldn't reset
in file name and full path mode.
Since mpv sends an empty title when no file is loaded, we can use that.
Note that when a file has no title, mpv sends the file name as title.

Fixes: dffbc6bb1223920ba8c03acd0d9011c57e3de8e5 ("Simplify the way the
title bar is set")

src/mainwindow.cpp
@@ -1976,6 +1976,8 @@ void MainWindow::setMediaTitle(const QString &title)
             newTitle.clear();
             break;
     }
+    if (title.isEmpty())
+        newTitle = QString();
     if (!newTitle.isEmpty())
         windowTitle = newTitle;
 