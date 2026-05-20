0db4190f3d0158985bf5570e2454016b63e8b62f
coolvitto <coolzdaddy55@gmail.com>
2026-05-20 09:37:59 +0200

    translations: Translated using Weblate (Japanese)
    Currently translated at 100.0% (1065 of 1065 strings)
    
    Translation: Media Player Classic Qute Theater/mpc-qt-translations
    Translate-URL: https://hosted.weblate.org/projects/mpc-qt/mpc-qt-translations/ja/

--- translations/mpc-qt_ja.ts
+++ translations/mpc-qt_ja.ts
@@ -101,7 +101,7 @@
     </message>
     <message>
         <source>16:9 Aspect ratio</source>
-        <translation type="unfinished"></translation>
+        <translation>16:9 アスペクト比</translation>
     </message>
     <message>
         <source>Decrease Aspect ratio</source>
