mpc-qt/mpc-qt

commit 2868d2a1327abc02589e6e25b91e7d2362a5f4e4

mpvwidget: Force mpv to clear last frame of previous video file
When loading a new video file, mpv keeps the last frame of the previous
one until it actually begins playing the new one.
Before 3eccf09b2e4ee621b733ec3653788314e555e99e, it led to blinking
(last frame -> logo image -> last frame -> new frame).
Since then, it led to a frozen last frame until the new frame.

By disabling the video track then re-enabling it, we force mpv to only
output a black frame until it has decoded the first new frame.
Combined with 3eccf09b2e4ee621b733ec3653788314e555e99e, this leads to a
faster video transition without lingering last frame.

src/mpvwidget.cpp
@@ -345,10 +345,12 @@ void MpvObject::urlOpen(QUrl url)
 void MpvObject::fileOpen(QString filename, bool replaceMpvPlaylist)
 {
     clearSubFiles();
+    setMpvPropertyVariant("vid", "no");
     if (replaceMpvPlaylist)
         emit ctrlCommand(QStringList({"loadfile", filename}));
     else
         emit ctrlCommand(QStringList({"loadfile", filename, "append-play"}));
+    setMpvPropertyVariant("vid", "auto");
     setMouseHideTime(hideTimer->interval());
 }
 

commit bc3abca8dcf338dfcce7d2ddacca14d4705f644e

Only use gapless playback for audio-to-audio transitions
Gapless playback relies on the mpv playlist, and is more prone to edge
cases.

Besides the issue fixed by 0df29a39e53325371808800ccea21d2f78a13907,
disabling video before loading a file and re-enabling it afterwards
(2868d2a1327abc02589e6e25b91e7d2362a5f4e4) causes mpv to report
EOF for the new file.

Since gapless playback is only useful when transitioning between
audio files, enable it only when opening an audio file while another
audio file is already playing.

Part 2 of fixing #982 ("Toggling yadif causes mpc-qt to overwrite the
wrong playlist entry").

Fixes: 2868d2a1327abc02589e6e25b91e7d2362a5f4e4 ("mpvwidget: Force mpv
to clear last frame of previous video file")

src/manager.cpp
@@ -726,7 +726,8 @@ void PlaybackManager::startPlayWithUuid(QUrl what, QUuid playlistUuid,
         emit startedPlayingFile(what);
     mpvObject_->fileOpen(what.isLocalFile() ? what.toLocalFile()
                                             : QUrl::fromPercentEncoding(what.toEncoded()),
-                         replaceMpvPlaylist);
+                         replaceMpvPlaylist,
+                         !videoList.isEmpty() && !videoListData[1].isImage);
     if (!with.isEmpty())
         mpvObject_->setSubFile(with.toString());
     mpvObject_->setPaused(playbackStartPaused);

src/mpvwidget.cpp
@@ -342,15 +342,17 @@ void MpvObject::urlOpen(QUrl url)
                                : QUrl::fromPercentEncoding(url.toEncoded()));
 }
 
-void MpvObject::fileOpen(QString filename, bool replaceMpvPlaylist)
+void MpvObject::fileOpen(QString filename, bool replaceMpvPlaylist, bool previousWasVideo)
 {
     clearSubFiles();
-    setMpvPropertyVariant("vid", "no");
-    if (replaceMpvPlaylist)
+    if (replaceMpvPlaylist || previousWasVideo) {
+        setMpvPropertyVariant("vid", "no"); // clear last frame of previous video file
         emit ctrlCommand(QStringList({"loadfile", filename}));
-    else
+        setMpvPropertyVariant("vid", "auto");
+    }
+    else { // gapless playback
         emit ctrlCommand(QStringList({"loadfile", filename, "append-play"}));
-    setMpvPropertyVariant("vid", "auto");
+    }
     setMouseHideTime(hideTimer->interval());
 }
 

src/mpvwidget.h
@@ -55,7 +55,7 @@ public:
     int selectedStatsPage();
 
     void urlOpen(QUrl url);
-    void fileOpen(QString filename, bool replaceMpvPlaylist = true);
+    void fileOpen(QString filename, bool replaceMpvPlaylist = true, bool previousWasVideo = false);
     void discFilesOpen(QString path);
     void stopPlayback();
     void stepBackward();