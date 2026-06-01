2de2cf7867eab4732d4b8c2c2d14a21a982cb0d9
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2026-06-01 13:33:28 +0200

    videopreview: Add fallback formats for yt-dlp
    With outdated versions of yt-dlp, less formats are sometimes available.

--- src/widgets/videopreview.cpp
+++ src/widgets/videopreview.cpp
@@ -23,7 +23,7 @@ VideoPreview::VideoPreview(QWidget *parent) : QWidget(parent)
     emit mpv->ctrlSetOptionVariant("hr-seek", "no");
     emit mpv->ctrlSetOptionVariant("audio", "no");
     emit mpv->ctrlSetOptionVariant("audio-display", "no");
-    emit mpv->ctrlSetOptionVariant("ytdl-format", QString("bestvideo[height>=?%1]").arg(videoHeight));
+    emit mpv->ctrlSetOptionVariant("ytdl-format", QString("bestvideo[height>=?%1]/best[height>=?%1]/bestvideo/best").arg(videoHeight));
     emit mpv->ctrlSetOptionVariant("ytdl-raw-options", "format-sort=[+size,+br,+res,+fps]");
     emit mpv->ctrlSetOptionVariant("clipboard-backends", "clr");
 
