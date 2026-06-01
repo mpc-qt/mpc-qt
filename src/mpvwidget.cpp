mpc-qt/mpc-qt

commit 848f86dea7a24938cc9166486c1ec4022d5450fb

mpvwidget: Clear audio/video filters before we set them
That way, when can actually remove filters.

src/mpvwidget.cpp
@@ -417,11 +417,13 @@ void MpvObject::setLogoBackground(const QColor &color)
 
 void MpvObject::setAudioFilters(const QList<QPair<QString, QString>> &filtersList)
 {
+    emit ctrlCommand("no-osd af clear");
     emit ctrlCommand("no-osd af set \"" + formatFiltersList(filtersList) + "\"");
 }
 
 void MpvObject::setVideoFilters(const QList<QPair<QString, QString>> &filtersList)
 {
+    emit ctrlCommand("no-osd vf clear");
     emit ctrlCommand("no-osd vf set \"" + formatFiltersList(filtersList) + "\"");
 }
 

commit 0d2cff05efe02836640461bf46360c6437ba9690

Only clear filters when needed
Avoid clearing filters when none are set, as mpv reports an error in
that case.

Fixes: 848f86dea7a24938cc9166486c1ec4022d5450fb ("mpvwidget: Clear
audio/video filters before we set them")

src/mpvwidget.cpp
@@ -432,16 +432,18 @@ void MpvObject::setLogoBackground(const QColor &color)
         widget->setLogoBackground(color);
 }
 
-void MpvObject::setAudioFilters(const QList<QPair<QString, QString>> &filtersList)
+void MpvObject::setAudioFilters(const QList<QPair<QString, QString>> &filtersList, bool clearFilters)
 {
-    emit ctrlCommand("no-osd af clear");
+    if (clearFilters)
+        emit ctrlCommand("no-osd af clear");
     if (!filtersList.empty())
         emit ctrlCommand("no-osd af set \"" + formatFiltersList(filtersList) + "\"");
 }
 
-void MpvObject::setVideoFilters(const QList<QPair<QString, QString>> &filtersList)
+void MpvObject::setVideoFilters(const QList<QPair<QString, QString>> &filtersList, bool clearFilters)
 {
-    emit ctrlCommand("no-osd vf clear");
+    if (clearFilters)
+        emit ctrlCommand("no-osd vf clear");
     if (!filtersList.empty())
         emit ctrlCommand("no-osd vf set \"" + formatFiltersList(filtersList) + "\"");
 }

src/mpvwidget.h
@@ -66,8 +66,8 @@ public:
     void setIsInBottomArea(bool entered);
     void setLogoUrl(const QString &filename);
     void setLogoBackground(const QColor &color);
-    void setAudioFilters(const QList<QPair<QString, QString>> &filtersList);
-    void setVideoFilters(const QList<QPair<QString, QString>> &filtersList);
+    void setAudioFilters(const QList<QPair<QString, QString>> &filtersList, bool clearFilters);
+    void setVideoFilters(const QList<QPair<QString, QString>> &filtersList, bool clearFilters);
     QString formatFiltersList(const QList<QPair<QString, QString>> &filtersList);
     void setSubFile(QString filename);
     void addSubFile(QString filename);

src/settingswindow.cpp
@@ -1193,17 +1193,17 @@ void SettingsWindow::setHidePanels(bool hidden)
 
 void SettingsWindow::setAudioFilter(QString filter, QString options, bool add)
 {
-    setFilter(audioFiltersList, filter, options, add);
-    emit audioFilters(audioFiltersList);
+    bool clearFilters = setFilter(audioFiltersList, filter, options, add);
+    emit audioFilters(audioFiltersList, clearFilters);
 }
 
 void SettingsWindow::setVideoFilter(QString filter, QString options, bool add)
 {
-    setFilter(videoFiltersList, filter, options, add);
-    emit videoFilters(videoFiltersList);
+    bool clearFilters = setFilter(videoFiltersList, filter, options, add);
+    emit videoFilters(videoFiltersList, clearFilters);
 }
 
-void SettingsWindow::setFilter(QList<QPair<QString, QString>> &filtersList, QString filter, QString options, bool add)
+bool SettingsWindow::setFilter(QList<QPair<QString, QString>> &filtersList, QString filter, QString options, bool add)
 {
     auto it = std::find_if(filtersList.begin(),
                                      filtersList.end(),
@@ -1211,7 +1211,8 @@ void SettingsWindow::setFilter(QList<QPair<QString, QString>> &filtersList, QStr
         return pair.first == filter;
     });
 
-    if (it != filtersList.end()) { // Filter found
+    bool filterFound = it != filtersList.end();
+    if (filterFound) {
         int i = std::distance(filtersList.begin(), it);
         if (add)
             filtersList.replace(i, QPair<QString, QString>(filter, options));
@@ -1220,6 +1221,8 @@ void SettingsWindow::setFilter(QList<QPair<QString, QString>> &filtersList, QStr
     }
     else if (add)
         filtersList.append(QPair<QString, QString>(filter, options));
+
+    return filterFound;
 }
 
 void SettingsWindow::setCustomMpvOptions()

src/settingswindow.h
@@ -196,8 +196,8 @@ signals:
     void logDelay(int msecs);
     void logHistory(int lines);
 
-    void audioFilters(const QList<QPair<QString, QString>> &audioFiltersList);
-    void videoFilters(const QList<QPair<QString, QString>> &videoFiltersList);
+    void audioFilters(const QList<QPair<QString, QString>> &audioFiltersList, bool clearFilters);
+    void videoFilters(const QList<QPair<QString, QString>> &videoFiltersList, bool clearFilters);
 
 public slots:
     void takeActions(const QList<QAction*> actions);
@@ -218,7 +218,7 @@ public slots:
     void setHidePanels(bool hidden);
 
 private slots:
-    void setFilter(QList<QPair<QString, QString>> &filtersList, QString filter, QString options, bool add);
+    bool setFilter(QList<QPair<QString, QString>> &filtersList, QString filter, QString options, bool add);
     void setCustomMpvOptions();
     void colorPick_clicked(QLineEdit *colorValue);
     void colorPick_changed(const QLineEdit *colorValue, QPushButton *colorPick);