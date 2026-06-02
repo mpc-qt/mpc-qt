d9b53dd7f4cad376bbacb9bacb165063b81a0450
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2026-06-02 09:48:43 +0200

    videopreview: Add QuickJS support for yt-dlp and enable auto EJS update
    Same as 505583561bc1f43e7fc7bcaa3ca4980e07e95c77 but for the seek bar
    video preview.

--- src/widgets/videopreview.cpp
+++ src/widgets/videopreview.cpp
@@ -24,7 +24,9 @@ VideoPreview::VideoPreview(QWidget *parent) : QWidget(parent)
     emit mpv->ctrlSetOptionVariant("audio", "no");
     emit mpv->ctrlSetOptionVariant("audio-display", "no");
     emit mpv->ctrlSetOptionVariant("ytdl-format", QString("bestvideo[height>=?%1]/best[height>=?%1]/bestvideo/best").arg(videoHeight));
-    emit mpv->ctrlSetOptionVariant("ytdl-raw-options", "format-sort=[+size,+br,+res,+fps]");
+    emit mpv->ctrlSetOptionVariant("ytdl-raw-options", "js-runtimes=quickjs,"\
+                                                    "remote-components=ejs:github,"\
+                                                    "format-sort=[+size,+br,+res,+fps]");
     emit mpv->ctrlSetOptionVariant("clipboard-backends", "clr");
 
     connect(mpv, &MpvObject::aspectChanged,
