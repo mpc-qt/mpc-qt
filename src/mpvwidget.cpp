mpc-qt/mpc-qt

commit 8d8ba9269eeaafe73bf785bb192aa9d6b56965c6

mpvwidget: Add Windows long path support
Fixes #599 ("Cannot play media with a file path over 260 characters on a
network share").

src/mpvwidget.cpp
@@ -336,6 +336,20 @@ int MpvObject::selectedStatsPage()
     return shownStatsPage;
 }
 
+static QString normalizeWindowsPath(QString filename)
+{
+#ifdef Q_OS_WINDOWS
+    filename = QDir::toNativeSeparators(filename);
+    if (filename.size() >= 260 && !filename.startsWith(u"\\\\?\\")) {
+        if (filename.startsWith(u"\\\\"))
+            filename = u"\\\\?\\UNC\\" + filename.mid(2);
+        else
+            filename.prepend(u"\\\\?\\");
+    }
+#endif
+    return filename;
+}
+
 void MpvObject::urlOpen(QUrl url)
 {
     fileOpen(url.isLocalFile() ? url.toLocalFile()
@@ -345,6 +359,7 @@ void MpvObject::urlOpen(QUrl url)
 void MpvObject::fileOpen(QString filename, bool replaceMpvPlaylist, bool previousWasVideo)
 {
     clearSubFiles();
+    filename = normalizeWindowsPath(filename);
     if (replaceMpvPlaylist || previousWasVideo) {
         setMpvPropertyVariant("vid", "no"); // clear last frame of previous video file
         emit ctrlCommand(QStringList({"loadfile", filename}));

commit c7a351e890274115bc852fae962ffe8cf497a312

mpvwidget: Prevent compiler warning
isLocalFile is only used on Windows.

Fixes: 8d8ba9269eeaafe73bf785bb192aa9d6b56965c6 ("mpvwidget: Add Windows
long path support")

src/mpvwidget.cpp
@@ -336,7 +336,7 @@ int MpvObject::selectedStatsPage()
     return shownStatsPage;
 }
 
-static QString normalizeWindowsPath(QString filename, bool isLocalFile)
+static QString normalizeWindowsPath(QString filename, [[maybe_unused]] bool isLocalFile)
 {
 #ifdef Q_OS_WINDOWS
     if (!isLocalFile)