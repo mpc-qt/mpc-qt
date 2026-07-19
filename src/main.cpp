mpc-qt/mpc-qt

commit 11f9a573fb1237cd4c3bb5111a081707d77db19d

Apply language change immediately

src/main.cpp
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
     // Spin up the application
     Flow f;
     f.parseArgs();
-    f.setTranslation();
+    f.setTranslation(false);
     f.detectMode();
     if (f.earlyQuit())
         return 0;
@@ -445,12 +445,13 @@ void Flow::earlyPlatformOverride()
 }
 
 // Register the translations
-void Flow::setTranslation()
+void Flow::setTranslation(bool updateTranslations)
 {
     if (cliNoConfig)
         return;
     QLocale locale;
-    settings = storage.readVMap(fileSettings);
+    if (settings.empty())
+        settings = storage.readVMap(fileSettings);
     bool forceEnglish = settings.value("playerLanguageForceEnglish", false).toBool();
     if (forceEnglish)
         locale = QLocale("en");
@@ -460,6 +461,18 @@ void Flow::setTranslation()
 
     if (appTranslator.load(locale, "mpc-qt", "_", ":/i18n"))
         QApplication::installTranslator(&appTranslator);
+
+    if (updateTranslations) {
+        mainWindow->updateLanguage();
+        mainWindow->playlistWindow()->updateLanguage();
+        settingsWindow->updateLanguage();
+        propertiesWindow->updateLanguage();
+        favoritesWindow->updateLanguage();
+        gotoWindow->updateLanguage();
+        logWindow->updateLanguage();
+        libraryWindow->updateLanguage();
+        thumbnailerWindow->updateLanguage();
+    }
 }
 
 void Flow::readConfig()
@@ -1576,6 +1589,7 @@ void Flow::settingswindow_settingsData(const QVariantMap &settings)
     // The selected options have changed, so write them to disk
     this->settings = settings;
     writeConfig(true);
+    setTranslation(true);
     Logger::log(logModule, "settingswindow_settingsData");
 }
 

src/main.h
@@ -39,7 +39,7 @@ public:
     void init();
     int run();
     bool earlyQuit();
-    void setTranslation();
+    void setTranslation(bool updateTranslations);
     static void earlyPlatformOverride();
     static bool isNvidiaGPU();
 

src/settingswindow.cpp
@@ -381,6 +381,11 @@ void SettingsWindow::setWaylandOptions(bool isWayland, bool isWaylandMode)
     ui->tweaksPreferWayland->setEnabled(isWayland);
 }
 
+void SettingsWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void SettingsWindow::setupPageTree()
 {
     // Expand every item on pageTree

src/settingswindow.h
@@ -62,6 +62,7 @@ public:
     QVariantMap settings();
     QVariantMap keyMap();
     void setWaylandOptions(bool isWayland, bool isWaylandMode);
+    void updateLanguage();
 
 private:
     void setupPageTree();

src/favoriteswindow.cpp
@@ -25,6 +25,11 @@ FavoritesWindow::~FavoritesWindow()
     delete ui;
 }
 
+void FavoritesWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void FavoritesWindow::setFiles(const QList<TrackInfo> &tracks)
 {
     fileList->setTracks(tracks);

src/favoriteswindow.h
@@ -22,6 +22,7 @@ class FavoritesWindow : public QWidget
 public:
     explicit FavoritesWindow(QWidget *parent = nullptr);
     ~FavoritesWindow();
+    void updateLanguage();
 
 signals:
     void favoriteTracks(QList<TrackInfo> files, QList<TrackInfo> streams);

src/gotowindow.cpp
@@ -23,6 +23,11 @@ GoToWindow::~GoToWindow()
     delete ui;
 }
 
+void GoToWindow::updateLanguage()
+{
+    ui->retranslateUi(this);
+}
+
 void GoToWindow::init(double currentTime, double maxTime, double fps)
 {
     maxTime_ = maxTime;

src/gotowindow.h
@@ -14,6 +14,7 @@ class GoToWindow : public QWidget
 public:
     explicit GoToWindow(QWidget *parent = nullptr);
     ~GoToWindow();
+    void updateLanguage();
 
 signals:
     void goTo(double time);

commit 0e6e59839b93d3fb1d4989b2688d52f2e553f2cc

Don't retranslate UI when changing zoom or panels settings
Retranslating the UI resets dynamic strings (like the window title).

Applying settings from the settings window updates the window title
through MainWindow::setTitleBarFormat, so it's not permanently affected
by the UI retranslation.

Fixes: 11f9a573fb1237cd4c3bb5111a081707d77db19d ("Apply language change
immediately")

Fixes #1018 ("Filename disappears from title bar when you hotkey zoom
the window").

src/main.cpp
@@ -1634,13 +1634,14 @@ void Flow::mpcHcServer_fileSelected(QString fileName)
     playbackManager->openSeveralFiles(urls, true);
 }
 
-void Flow::settingswindow_settingsData(const QVariantMap &settings)
+void Flow::settingswindow_settingsData(const QVariantMap &settings, bool updateTranslation)
 {
     Logger::log(logModule, "settingswindow_settingsData");
     // The selected options have changed, so write them to disk
     this->settings = settings;
     writeConfig(true);
-    setTranslation(true);
+    if (updateTranslation)
+        setTranslation(true);
 }
 
 void Flow::settingswindow_inhibitScreensaver(bool yes)

src/main.h
@@ -94,7 +94,7 @@ private slots:
     void manager_startedPlayingFile(QUrl url);
     void manager_stoppedPlaying();
     void mpcHcServer_fileSelected(QString fileName);
-    void settingswindow_settingsData(const QVariantMap &settings);
+    void settingswindow_settingsData(const QVariantMap &settings, bool updateTranslation);
     void settingswindow_inhibitScreensaver(bool yes);
     void settingswindow_rememberHistory(bool yes, bool onlyVideos);
     void settingswindow_rememberFilePosition(bool yes);

src/settingswindow.cpp
@@ -1128,7 +1128,7 @@ void SettingsWindow::sendSignals()
 
 void SettingsWindow::sendAcceptedSettings()
 {
-    emit settingsData(acceptedSettings.toVMap());
+    emit settingsData(acceptedSettings.toVMap(), true);
     emit keyMapData(acceptedKeyMap);
 }
 
@@ -1185,14 +1185,14 @@ void SettingsWindow::setZoomPreset(int which)
     ui->playbackAutoZoom->setChecked(autoZoom);
     ui->playbackAutoZoomMethod->setCurrentIndex(zoomMethod);
 
-    emit settingsData(acceptedSettings.toVMap());
+    emit settingsData(acceptedSettings.toVMap(), false);
 }
 
 void SettingsWindow::setHidePanels(bool hidden)
 {
     ui->fullscreenHidePanels->setChecked(hidden);
     WIDGET_LOOKUP(ui->fullscreenHidePanels).setValue(hidden);
-    emit settingsData(acceptedSettings.toVMap());
+    emit settingsData(acceptedSettings.toVMap(), false);
 }
 
 void SettingsWindow::setAudioFilter(QString filter, QString options, bool add)

src/settingswindow.h
@@ -81,7 +81,7 @@ private:
     int pageIndexForTreeItem(const QModelIndex &index) const;
 
 signals:
-    void settingsData(const QVariantMap &s);
+    void settingsData(const QVariantMap &s, bool updateTranslation);
     void keyMapData(const QVariantMap &s);
     void mouseWindowedMap(const MouseStateMap &map);
     void mouseFullscreenMap(const MouseStateMap &map);