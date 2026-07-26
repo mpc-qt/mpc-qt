mpc-qt/mpc-qt

commit d62beb794a5655b709c8b96ad4d8c1a64d16550a

Update icons and colors on system theme or palette change
The built-in icons were not getting updated.
Same thing for colors for drawnslider widget on system theme change and
tooltip and videopreview widgets for both system theme and palette
change.

src/widgets/tooltip.cpp
@@ -1,4 +1,4 @@
-#include <QToolTip>
+#include <QApplication>
 #include "tooltip.h"
 
 static constexpr char logModule[] =  "tooltip";
@@ -11,19 +11,7 @@ Tooltip::Tooltip(QWidget *parent) : QWidget(parent)
     textLabel->setAlignment(Qt::AlignCenter);
 
     textLabel->setAutoFillBackground(true);
-    QPalette tooltipPalette = QToolTip::palette();
-    QPalette customPalette = this->palette();
-    customPalette.setColor(QPalette::Window, tooltipPalette.color(QPalette::ToolTipBase));
-    customPalette.setColor(QPalette::WindowText, tooltipPalette.color(QPalette::ToolTipText));
-    textLabel->setPalette(customPalette);
-    textLabel->setStyleSheet(R"(
-        QLabel {
-            background-color: palette(Window);
-            color: palette(WindowText);
-            border: 1px solid palette(Light);
-            border-radius: 4px;
-        }
-    )");
+    updatePalette();
 
     hide();
     this->setVisible(false);
@@ -33,6 +21,22 @@ Tooltip::~Tooltip()
 {
     delete textLabel;
     textLabel = nullptr;
+}
+
+void Tooltip::updatePalette()
+{
+    QPalette palette = QApplication::palette();
+    QString css = QString(
+        "background-color: %1;"
+        "color: %2;"
+        "border: 1px solid %3;"
+        "border-radius: 4px;"
+    )
+    .arg(palette.color(QPalette::ToolTipBase).name(),
+        palette.color(QPalette::ToolTipText).name(),
+        palette.color(QPalette::Mid).name());
+
+    textLabel->setStyleSheet(css);
 }
 
 void Tooltip::show(const QString &text, const QPoint &where, int mainWindowWidth, const QString &textTemplate)

src/helpers.cpp
@@ -780,6 +780,11 @@ void IconThemer::setIconFolders(FolderMode folderMode, const QString &customFold
         updateButton(data);
 }
 
+void IconThemer::updateIcons()
+{
+    setIconFolders(folderMode_, customFolder_);
+}
+
 void IconThemer::updateButton(const IconData &data)
 {
     QString nameToUse = data.iconNormal;

src/helpers.h
@@ -90,6 +90,7 @@ public:
     QIcon fetchIcon(const QString &name);
     void updateButton(const IconData &data);
     void setIconFolders(IconThemer::FolderMode folderMode, const QString &customFolder);
+    void updateIcons();
 
 private:
     QList<IconData> iconDataList;

src/mainwindow.cpp
@@ -376,6 +376,21 @@ void MainWindow::resizeEvent(QResizeEvent *event)
     resizePlaylistToFit();
 }
 
+void MainWindow::changeEvent(QEvent *event)
+{
+    if (event->type() == QEvent::ThemeChange || event->type() == QEvent::PaletteChange) {
+        positionSlider_->applicationPaletteChanged();
+        volumeSlider_->applicationPaletteChanged();
+        themer.updateIcons();
+        playlistWindow_->updateIcons();
+        ui->menubar->setStyle(ui->menubar->style());
+        if (tooltip)
+            tooltip->updatePalette();
+        if (videoPreview)
+            videoPreview->updatePalette();
+    }
+}
+
 bool MainWindow::eventFilter(QObject *object, QEvent *event)
 {
     bool insideMpv = mpvw ? object == mpvw : false;
@@ -390,9 +405,6 @@ bool MainWindow::eventFilter(QObject *object, QEvent *event)
         else if (event->type() == QEvent::Enter)
             mpvObject_->setIsInBottomArea(true);
     }
-
-    if (event->type() == QEvent::ApplicationPaletteChange)
-        positionSlider_->applicationPaletteChanged();
 
     if (Platform::isWindows && event->type() == QEvent::KeyPress) {
         QKeyEvent *ke = static_cast<QKeyEvent*>(event);

src/mainwindow.h
@@ -53,6 +53,7 @@ public:
 
 protected:
     void resizeEvent(QResizeEvent *event);
+    void changeEvent(QEvent *event) override;
     bool eventFilter(QObject *object, QEvent *event);
     void closeEvent(QCloseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);

src/playlistwindow.cpp
@@ -295,6 +295,11 @@ void PlaylistWindow::tabsFromVList(const QVariantList &qvl)
     updatePlaylistHasItems();
 }
 
+void PlaylistWindow::updateIcons()
+{
+    themer.updateIcons();
+}
+
 bool PlaylistWindow::eventFilter(QObject *obj, QEvent *event)
 {
     Q_UNUSED(obj)

src/playlistwindow.h
@@ -47,6 +47,8 @@ public:
     QVariantList tabsToVList(bool saveQuickPlaylist) const;
     void tabsFromVList(const QVariantList &qvl);
 
+    void updateIcons();
+
 protected:
     bool eventFilter(QObject *obj, QEvent *event);
     void dragEnterEvent(QDragEnterEvent *event);

src/widgets/tooltip.h
@@ -7,6 +7,7 @@ class Tooltip : public QWidget {
     public:
         explicit Tooltip(QWidget *parent = nullptr);
         ~Tooltip();
+        void updatePalette();
         void show(const QString &text, const QPoint &where, int mainWindowWidth, const QString &textTemplate);
         void hide();
 

commit 180b771d3164d3cad90ab7873d81c26ae9dd904c

tooltip: Add support for transparency
Fixes: d62beb794a5655b709c8b96ad4d8c1a64d16550a ("Update icons and
colors on system theme or palette change")
Fixes: fd185872906e1265b97f9e65a4c1601cbe7f3d8e ("Add support for
transparency to all color settings")

src/widgets/tooltip.cpp
@@ -35,9 +35,9 @@ void Tooltip::updatePalette()
         "border: 1px solid %3;"
         "border-radius: 4px;"
     )
-    .arg(palette.color(QPalette::ToolTipBase).name(),
-        palette.color(QPalette::ToolTipText).name(),
-        palette.color(darkPalette ? QPalette::Light : QPalette::Mid).name());
+    .arg(palette.color(QPalette::ToolTipBase).name(QColor::HexArgb),
+        palette.color(QPalette::ToolTipText).name(QColor::HexArgb),
+        palette.color(darkPalette ? QPalette::Light : QPalette::Mid).name(QColor::HexArgb));
 
     textLabel->setStyleSheet(css);
 }