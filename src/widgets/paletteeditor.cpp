mpc-qt/mpc-qt

commit 000c30690567c355b0e281b37489b2f530b8d77f

paletteeditor: Fix building with Qt < 6.6
QPalette::Accent requires Qt 6.6.

src/widgets/paletteeditor.cpp
@@ -30,9 +30,13 @@ Q_GLOBAL_STATIC_WITH_ARGS(RoleLabels, roleText, ({
     { QPalette::AlternateBase, QObject::tr("Base (alternate)") },
     { QPalette::NoRole, QObject::tr("No role") },
     { QPalette::ToolTipBase, QObject::tr("Tooltip base") },
+#if QT_VERSION >= QT_VERSION_CHECK(6,6,0)
     { QPalette::ToolTipText, QObject::tr("Tooltip text") },
     { QPalette::PlaceholderText, QObject::tr("Placeholder text") },
     { QPalette::Accent, QObject::tr("Accent") }
+#else
+    { QPalette::ToolTipText, QObject::tr("Tooltip text") }
+#endif
 }));
 
 using GroupLabels = QList<QPair<QPalette::ColorGroup, QString>>;

commit 8994e3521db8281ddeac90535d8d9faa4256c456

paletteeditor: Fix missing PlaceholderText on Qt < 6.6
Fixes: 000c30690567c355b0e281b37489b2f530b8d77f ("paletteeditor: Fix
building with Qt < 6.6")

src/widgets/paletteeditor.cpp
@@ -30,12 +30,12 @@ Q_GLOBAL_STATIC_WITH_ARGS(RoleLabels, roleText, ({
     { QPalette::AlternateBase, QObject::tr("Base (alternate)") },
     { QPalette::NoRole, QObject::tr("No role") },
     { QPalette::ToolTipBase, QObject::tr("Tooltip base") },
-#if QT_VERSION >= QT_VERSION_CHECK(6,6,0)
     { QPalette::ToolTipText, QObject::tr("Tooltip text") },
+#if QT_VERSION >= QT_VERSION_CHECK(6,6,0)
     { QPalette::PlaceholderText, QObject::tr("Placeholder text") },
     { QPalette::Accent, QObject::tr("Accent") }
 #else
-    { QPalette::ToolTipText, QObject::tr("Tooltip text") }
+    { QPalette::PlaceholderText, QObject::tr("Placeholder text") }
 #endif
 }));
 