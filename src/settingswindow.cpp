mpc-qt/mpc-qt

commit c03dc4625d08b5b18f41d0b37621fa64fc4fdb46

Add "Close / Minimize to tray" feature
Requires the tray icon and single window mode to be enabled.

The Exit command and middle-clicking the tray icon (newly added) always
close MPC-QT.

Minimize to tray doesn't work (yet?) on Wayland in Wayland mode, it
requires the Wayland compositor to let the window know that it's
minimized.

Fixes #1053 ("Minimize to tray and keep running in tray?").

src/settingswindow.cpp
@@ -719,6 +719,8 @@ void SettingsWindow::sendSignals()
 
     emit appendToQuickPlaylist(WIDGET_LOOKUP(ui->playerAppendToQuickPlaylist).toBool());
     emit trayIcon(WIDGET_LOOKUP(ui->playerTrayIcon).toBool());
+    emit closeToTray(WIDGET_LOOKUP(ui->playerCloseToTray).toBool());
+    emit minimizeToTray(WIDGET_LOOKUP(ui->playerMinimizeToTray).toBool());
     emit showOsd(WIDGET_LOOKUP(ui->playerOSD).toBool());
     emit limitProportions(WIDGET_LOOKUP(ui->playerLimitProportions).toBool());
     emit disableOpenDiscMenu(WIDGET_LOOKUP(ui->playerDisableOpenDisc).toBool());
@@ -1401,8 +1403,19 @@ void SettingsWindow::keyPressEvent(QKeyEvent *event)
 void SettingsWindow::on_playerOpenNew_toggled(bool checked)
 {
     ui->playerAppendToQuickPlaylist->setEnabled(!checked);
-    if (checked)
+    if (checked) {
         ui->playerAppendToQuickPlaylist->setChecked(false);
+        ui->playerCloseToTray->setChecked(false);
+        ui->playerMinimizeToTray->setChecked(false);
+    }
+}
+
+void SettingsWindow::on_playerTrayIcon_toggled(bool checked)
+{
+    if (!checked) {
+        ui->playerCloseToTray->setChecked(false);
+        ui->playerMinimizeToTray->setChecked(false);
+    }
 }
 
 void SettingsWindow::on_playerAppendToQuickPlaylist_toggled(bool checked)

src/settingswindow.h
@@ -88,6 +88,8 @@ signals:
 
     void appendToQuickPlaylist(bool yes);
     void trayIcon(bool yes);
+    void closeToTray(bool yes);
+    void minimizeToTray(bool yes);
     void showOsd(bool yes);
     void limitProportions(bool yes);
     void disableOpenDiscMenu(bool yes);
@@ -235,6 +237,8 @@ private slots:
     bool eventFilter(QObject *obj, QEvent *event) override;
 
     void on_playerOpenNew_toggled(bool checked);
+
+    void on_playerTrayIcon_toggled(bool checked);
 
     void on_playerAppendToQuickPlaylist_toggled(bool checked);
 

src/settingswindow.ui
@@ -10,7 +10,7 @@
     <x>0</x>
     <y>0</y>
     <width>896</width>
-    <height>638</height>
+    <height>666</height>
    </rect>
   </property>
   <property name="sizePolicy">
@@ -445,6 +445,20 @@ media file played</string>
                <widget class="QCheckBox" name="playerTrayIcon">
                 <property name="text">
                  <string>Tray icon</string>
+                </property>
+               </widget>
+              </item>
+              <item>
+               <widget class="QCheckBox" name="playerMinimizeToTray">
+                <property name="text">
+                 <string>Minimize to tray</string>
+                </property>
+               </widget>
+              </item>
+              <item>
+               <widget class="QCheckBox" name="playerCloseToTray">
+                <property name="text">
+                 <string>Close to tray</string>
                 </property>
                </widget>
               </item>
@@ -8761,6 +8775,22 @@ media file played</string>
     <hint type="destinationlabel">
      <x>537</x>
      <y>149</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>playerTrayIcon</sender>
+   <signal>toggled(bool)</signal>
+   <receiver>playerCloseToTray</receiver>
+   <slot>setEnabled(bool)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>718</x>
+     <y>84</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>718</x>
+     <y>112</y>
     </hint>
    </hints>
   </connection>

src/main.cpp
@@ -731,6 +731,10 @@ void Flow::setupSettingsConnections()
     // settings -> mainwindow
     connect(settingsWindow, &SettingsWindow::trayIcon,
             mainWindow, &MainWindow::setTrayIcon);
+    connect(settingsWindow, &SettingsWindow::closeToTray,
+            mainWindow, &MainWindow::setCloseToTray);
+    connect(settingsWindow, &SettingsWindow::minimizeToTray,
+            mainWindow, &MainWindow::setMinimizeToTray);
     connect(settingsWindow, &SettingsWindow::titleBarFormat,
             mainWindow, &MainWindow::setTitleBarFormat);
     connect(settingsWindow, &SettingsWindow::mouseWindowedMap,

src/mainwindow.cpp
@@ -402,8 +402,14 @@ void MainWindow::changeEvent(QEvent *event)
             videoPreview->updatePalette();
         ui->statusTime->updatePalette();
     }
-    else if (event->type() == QEvent::WindowStateChange && isMaximized())
-        emit windowMaximized();
+    else if (event->type() == QEvent::WindowStateChange) {
+        if (isMaximized())
+            emit windowMaximized();
+        else if (isMinimized() && minimizeToTray) {
+            isHiddenToTray = true;
+            hide();
+        }
+    }
 }
 
 void MainWindow::moveEvent(QMoveEvent *event)
@@ -459,6 +465,12 @@ bool MainWindow::eventFilter(QObject *object, QEvent *event)
 void MainWindow::closeEvent(QCloseEvent *event)
 {
     Logger::log(logModule, "closeEvent");
+    if (closeToTray && !isHiddenToTray && !reallyClose) {
+        hide();
+        isHiddenToTray = true;
+        event->ignore();
+        return;
+    }
     bool showPlaylist = ui->actionViewHidePlaylist->isChecked();
     playlistWindow_->close();
     ui->actionViewHidePlaylist->setChecked(showPlaylist);
@@ -733,6 +745,8 @@ void MainWindow::setupTrayIcon()
     Logger::log(logModule, "rendering trayIcon sizes");
     trayIcon->setIcon(createIconFromSvg(mpcQtIconPath, 64));
     Logger::log(logModule, "rendering trayIcon sizes done");
+    connect(trayIcon, &QSystemTrayIcon::activated,
+            this, &MainWindow::trayIcon_activated);
 }
 
 void MainWindow::setupActionGroups()
@@ -1817,6 +1831,16 @@ void MainWindow::setTrayIcon(bool enabled)
         trayIcon->hide();
 }
 
+void MainWindow::setCloseToTray(bool enabled)
+{
+    closeToTray = enabled;
+}
+
+void MainWindow::setMinimizeToTray(bool enabled)
+{
+    minimizeToTray = enabled;
+}
+
 void MainWindow::setTitleBarFormat(Helpers::TitlePrefix titlebarFormat)
 {
     titlebarFormat_ = titlebarFormat;
@@ -2709,6 +2733,7 @@ void MainWindow::on_actionFileClose_triggered()
 
 void MainWindow::on_actionFileExit_triggered()
 {
+    reallyClose = true;
     close();
 }
 
@@ -3670,6 +3695,17 @@ void MainWindow::hideTimer_timeout()
     if (fullscreenMode_ &&
             !ui->bottomArea->geometry().contains(mpvw->mapFromGlobal(QCursor::pos())))
         ui->bottomArea->hide();
+}
+
+void MainWindow::trayIcon_activated(QSystemTrayIcon::ActivationReason reason)
+{
+    if (reason == QSystemTrayIcon::Trigger || reason == QSystemTrayIcon::DoubleClick) {
+        isHiddenToTray ? show() : hide();
+        isHiddenToTray = !isHiddenToTray;
+    } else if (reason == QSystemTrayIcon::MiddleClick) {
+        reallyClose = true;
+        close();
+    }
 }
 
 void MainWindow::on_actionPlaylistRemoveSelected_triggered()

src/mainwindow.h
@@ -250,6 +250,8 @@ public slots:
     void setFullscreenMode(bool fullscreenMode);
     void setNoVideoSize(const QSize &sz);
     void setTrayIcon(bool enabled);
+    void setCloseToTray(bool enabled);
+    void setMinimizeToTray(bool enabled);
     void setTitleBarFormat(Helpers::TitlePrefix titlebarFormat);
     void setWindowedMouseMap(const MouseStateMap &map);
     void setFullscreenMouseMap(const MouseStateMap &map);
@@ -491,6 +493,7 @@ private slots:
     void playlistWindow_windowDocked();
     void playlistWindow_playlistAddItem(const QUuid &playlistUuid);
     void hideTimer_timeout();
+    void trayIcon_activated(QSystemTrayIcon::ActivationReason reason);
 
     void on_actionFileLoadSubtitle_triggered();
 
@@ -525,6 +528,10 @@ private:
     QMenu *muteMenu = nullptr;
 
     bool freestanding_ = false;
+    bool closeToTray = false;
+    bool minimizeToTray = false;
+    bool isHiddenToTray = false;
+    bool reallyClose = false;
     Helpers::TitlePrefix titlebarFormat_ = Helpers::PrefixFileName;
     bool mainwindowIsClosing = false; // Prevents toggleViewAction from affecting saved setting for actionViewHidePlaylist
     DecorationState decorationState_ = AllDecorations;

translations/mpc-qt_ar.ts
@@ -4470,6 +4470,14 @@ media file played</source>
         <source>Show video preview (restart required), set its height to (% of screen):</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Minimize to tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close to tray</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

translations/mpc-qt_ca.ts
@@ -4872,6 +4872,14 @@ arxiu multimèdia reproduït</translation>
         <source>Show video preview (restart required), set its height to (% of screen):</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Minimize to tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close to tray</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>StatusTime</name>

commit 164e08195d95eae787a724135f12d9ded2889466

settingswindow: Fix "Close / Minimize to tray" settings
Disable the checkboxes when the tray icon isn't enabled.

Fixes: c03dc4625d08b5b18f41d0b37621fa64fc4fdb46 ("Add "Close / Minimize
to tray" feature")

src/settingswindow.cpp
@@ -719,8 +719,8 @@ void SettingsWindow::sendSignals()
 
     emit appendToQuickPlaylist(WIDGET_LOOKUP(ui->playerAppendToQuickPlaylist).toBool());
     emit trayIcon(WIDGET_LOOKUP(ui->playerTrayIcon).toBool());
-    emit closeToTray(WIDGET_LOOKUP(ui->playerCloseToTray).toBool());
-    emit minimizeToTray(WIDGET_LOOKUP(ui->playerMinimizeToTray).toBool());
+    emit closeToTray(ui->playerCloseToTray->isEnabled() && WIDGET_LOOKUP(ui->playerCloseToTray).toBool());
+    emit minimizeToTray(ui->playerMinimizeToTray->isEnabled() && WIDGET_LOOKUP(ui->playerMinimizeToTray).toBool());
     emit showOsd(WIDGET_LOOKUP(ui->playerOSD).toBool());
     emit limitProportions(WIDGET_LOOKUP(ui->playerLimitProportions).toBool());
     emit disableOpenDiscMenu(WIDGET_LOOKUP(ui->playerDisableOpenDisc).toBool());
@@ -1405,14 +1405,6 @@ void SettingsWindow::on_playerOpenNew_toggled(bool checked)
     ui->playerAppendToQuickPlaylist->setEnabled(!checked);
     if (checked) {
         ui->playerAppendToQuickPlaylist->setChecked(false);
-        ui->playerCloseToTray->setChecked(false);
-        ui->playerMinimizeToTray->setChecked(false);
-    }
-}
-
-void SettingsWindow::on_playerTrayIcon_toggled(bool checked)
-{
-    if (!checked) {
         ui->playerCloseToTray->setChecked(false);
         ui->playerMinimizeToTray->setChecked(false);
     }

src/settingswindow.h
@@ -238,8 +238,6 @@ private slots:
 
     void on_playerOpenNew_toggled(bool checked);
 
-    void on_playerTrayIcon_toggled(bool checked);
-
     void on_playerAppendToQuickPlaylist_toggled(bool checked);
 
     void on_playerKeepHistory_toggled(bool checked);

src/settingswindow.ui
@@ -450,6 +450,9 @@ media file played</string>
               </item>
               <item>
                <widget class="QCheckBox" name="playerMinimizeToTray">
+                <property name="enabled">
+                 <bool>false</bool>
+                </property>
                 <property name="text">
                  <string>Minimize to tray</string>
                 </property>
@@ -457,6 +460,9 @@ media file played</string>
               </item>
               <item>
                <widget class="QCheckBox" name="playerCloseToTray">
+                <property name="enabled">
+                 <bool>false</bool>
+                </property>
                 <property name="text">
                  <string>Close to tray</string>
                 </property>
@@ -8782,6 +8788,22 @@ media file played</string>
    <sender>playerTrayIcon</sender>
    <signal>toggled(bool)</signal>
    <receiver>playerCloseToTray</receiver>
+   <slot>setEnabled(bool)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>718</x>
+     <y>84</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>718</x>
+     <y>112</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>playerTrayIcon</sender>
+   <signal>toggled(bool)</signal>
+   <receiver>playerMinimizeToTray</receiver>
    <slot>setEnabled(bool)</slot>
    <hints>
     <hint type="sourcelabel">