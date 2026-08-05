mpc-qt/mpc-qt

commit 59d88ce187458b233b0b4b2686c7f72138654e8e

mainwindow: Don't start playing on click in bottom area in fullscreen
Fixes: e252ceeb07aad8aa47cbef9c6b3f6a3d5a71cdd5("Trigger Play from
stopped state when clicking on mpvWidget")
Fixes: 5500c653344fbd56aee8fb1499488f8f7a8ff51f ("mainwindow: Make Play
/ Pause action trigger Play button")

src/mainwindow.cpp
@@ -2442,7 +2442,7 @@ void MainWindow::libraryWindowClosed()
 
 void MainWindow::mpvObject_mouseReleased()
 {
-    if (!isPlaying) {
+    if (!isPlaying && !mousePressedInBottomArea) {
         emit playCurrentItemRequested();
         return;
     }
@@ -3161,7 +3161,8 @@ void MainWindow::on_actionViewOptions_triggered()
 
 void MainWindow::on_actionPlayPause_triggered()
 {
-    on_play_clicked();
+    if (!mousePressedInBottomArea)
+        on_play_clicked();
 }
 
 void MainWindow::on_actionPlayStop_triggered()

commit 812d00b705960bbf9a88ecf52fd219df70dddef9

mainwindow: Fix play / pause after closing context menus
Manually reset mousePressedInBottomArea.

After closing the context menus of the subtitles, mute buttons
(bfbe7fba2954682950f1e75b8c7ca196aaa24d86) and the status time
(8c9aab2f06bd705e459365ebdb540d579d296374), mousePressedInBottomArea
doesn't get reset in any of the mouse event handlers.

Fixes: 59d88ce187458b233b0b4b2686c7f72138654e8e ("mainwindow: Don't
start playing on click in bottom area in fullscreen")

src/mainwindow.cpp
@@ -890,7 +890,10 @@ void MainWindow::setupStatus()
 {
     ui->tinyicon->setPixmap(renderPixmapFromSvg(tinyIconPath));
     connect(ui->statusTime, &StatusTime::customContextMenuRequested,
-            ui->statusTime, &StatusTime::showContextMenu);
+            ui->statusTime, [this](const QPointF &p) {
+                ui->statusTime->showContextMenu(p);
+                mousePressedInBottomArea = false;
+            });
     connect(ui->statusTime, &StatusTime::doubleClicked,
             ui->actionNavigateGoto, &QAction::trigger);
 }
@@ -1388,11 +1391,13 @@ void MainWindow::showOsdTimer(bool onSeek)
 void MainWindow::showSubsMenu()
 {
     subsMenu->exec(QCursor::pos());
+    mousePressedInBottomArea = false;
 }
 
 void MainWindow::showMuteMenu()
 {
     muteMenu->exec(QCursor::pos());
+    mousePressedInBottomArea = false;
 }
 
 QList<QUrl> MainWindow::doQuickOpenFileDialog()