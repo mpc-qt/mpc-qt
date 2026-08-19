mpc-qt/mpc-qt

commit ae2952c0ac66b58bbaa96053231a5c9504a680c4

Display buffering state and progress in status bar on buffer underrun
Shows "Buffering (X%)" instead of "Playing" when the buffer needs to be
refilled.

The previous "Buffering" state which was shown when starting a file is
replaced by a new "Loading" state. The initial buffering state isn't
advertised by mpv.

src/manager.cpp
@@ -111,6 +111,10 @@ void PlaybackManager::setMpvObject(MpvObject *mpvObject, bool makeConnections)
                 this, &PlaybackManager::mpvw_seekableChanged);
         connect(mpvObject, &MpvObject::playbackLoading,
                 this, &PlaybackManager::mpvw_playbackLoading);
+        connect(mpvObject, &MpvObject::pausedForCacheChanged,
+                this, &PlaybackManager::mpvw_pausedForCache);
+        connect(mpvObject, &MpvObject::bufferStateChanged,
+                this, &PlaybackManager::mpvw_bufferFillStateChanged);
         connect(mpvObject, &MpvObject::playbackStarted,
                 this, &PlaybackManager::mpvw_playbackStarted);
         connect(mpvObject, &MpvObject::pausedChanged,
@@ -1014,8 +1018,24 @@ void PlaybackManager::mpvw_seekableChanged(bool yes)
 
 void PlaybackManager::mpvw_playbackLoading()
 {
-    playbackState_ = BufferingState;
+    playbackState_ = LoadingState;
     emit stateChanged(playbackState_);
+}
+
+void PlaybackManager::mpvw_pausedForCache(QString paused)
+{
+    if (paused == strTrue)
+        playbackState_ = BufferingState;
+    else if (paused == strFalse)
+        playbackState_ = PlayingState;
+    else
+        playbackState_ = StoppedState;
+    emit stateChanged(playbackState_);
+}
+
+void PlaybackManager::mpvw_bufferFillStateChanged(int64_t percentage)
+{
+    emit stateChanged(playbackState_, percentage);
 }
 
 void PlaybackManager::mpvw_playbackStarted()
@@ -1049,7 +1069,7 @@ void PlaybackManager::mpvw_playbackIdling(bool yes)
 }
 
 void PlaybackManager::mpvw_playbackFinished() {
-    if (playbackState_ == BufferingState) {
+    if (playbackState_ == LoadingState) {
         playbackState_ = StoppedState;
         emit stateChanged(playbackState_);
         mpvw_eofReachedChanged(strTrue);

src/mainwindow.cpp
@@ -2094,7 +2094,7 @@ void MainWindow::setFullscreenHidePanels(bool hidden)
     }
 }
 
-void MainWindow::setPlaybackState(PlaybackManager::PlaybackState state)
+void MainWindow::setPlaybackState(PlaybackManager::PlaybackState state, int64_t bufferFillState)
 {
     // Update the fullscreen state
     if (state == PlaybackManager::StoppedState) {
@@ -2108,11 +2108,26 @@ void MainWindow::setPlaybackState(PlaybackManager::PlaybackState state)
     }
 
     // Update the UI
-    ui->status->setText(state==PlaybackManager::StoppedState ? tr("Stopped") :
-                        state==PlaybackManager::PausedState ? tr("Paused") :
-                        state==PlaybackManager::PlayingState ? tr("Playing") :
-                        state==PlaybackManager::BufferingState ? tr("Buffering") :
-                                                                 tr("Unknown"));
+    switch (state) {
+    case PlaybackManager::StoppedState:
+        ui->status->setText(tr("Stopped"));
+        break;
+    case PlaybackManager::PausedState:
+        ui->status->setText(tr("Paused"));
+        break;
+    case PlaybackManager::PlayingState:
+        ui->status->setText(tr("Playing"));
+        break;
+    case PlaybackManager::LoadingState:
+        ui->status->setText(tr("Loading"));
+        break;
+    case PlaybackManager::BufferingState:
+        ui->status->setText(tr("Buffering (%1%)").arg(bufferFillState));
+        break;
+    case PlaybackManager::WaitingState:
+        ui->status->setText(tr("Unknown"));
+        break;
+    }
     isPlaying = state != PlaybackManager::StoppedState;
     isPaused = state == PlaybackManager::PausedState;
     setUiEnabledState(state != PlaybackManager::StoppedState);

src/mainwindow.h
@@ -273,7 +273,7 @@ public slots:
     void setTimeTooltip(bool show, bool above);
     void setOsdTimerOnSeek(bool enabled);
     void setFullscreenHidePanels(bool hidden);
-    void setPlaybackState(PlaybackManager::PlaybackState state);
+    void setPlaybackState(PlaybackManager::PlaybackState state, int64_t bufferFillState);
     void setPlaybackType(PlaybackManager::PlaybackType type);
     void disableChaptersMenus();
     void setChapters(QList<Chapter> chapters);

src/manager.h
@@ -48,7 +48,7 @@ class PlaybackManager : public QObject
     Q_OBJECT
 public:
     enum PlaybackState { StoppedState, PausedState, PlayingState,
-                         BufferingState, WaitingState };
+                         LoadingState, BufferingState, WaitingState };
     enum PlaybackType { None, File, Disc, Stream, Device };
     enum class Deinterlace { Yes, Auto, No };
 
@@ -66,7 +66,7 @@ signals:
     void titleChangedWithFilename(QString title, QString filename);
     void videoSizeChanged(QSize size);
     void playbackSpeedChanged(double speed);
-    void stateChanged(PlaybackManager::PlaybackState state);
+    void stateChanged(PlaybackManager::PlaybackState state, int bufferFillState = 0);
     void fileClosed();
     void typeChanged(PlaybackManager::PlaybackType type);
     // Transmit a map of chapter index to time,description pairs
@@ -209,6 +209,8 @@ private slots:
     void mpvw_playLengthChanged(double length);
     void mpvw_seekableChanged(bool yes);
     void mpvw_playbackLoading();
+    void mpvw_pausedForCache(QString paused);
+    void mpvw_bufferFillStateChanged(int64_t percentage);
     void mpvw_playbackStarted();
     void mpvw_pausedChanged(bool yes);
     void mpvw_playbackIdling(bool yes);

src/mpvwidget.cpp
@@ -68,7 +68,9 @@ MpvObject::PropertyDispatchMap MpvObject::propertyDispatch = {
     HANDLE_PROP("file-size", fileSizeChanged, toLongLong, 0ll),
     HANDLE_PROP("path", filePathChanged, toString, QString()),
     HANDLE_PROP("sub-text", subTextChanged, toString, QString()),
-    HANDLE_PROP("hwdec-current", hwdecCurrentChanged, toString, QString())
+    HANDLE_PROP("hwdec-current", hwdecCurrentChanged, toString, QString()),
+    HANDLE_PROP("paused-for-cache", pausedForCacheChanged, toString, QString()),
+    HANDLE_PROP("cache-buffering-state", bufferStateChanged, toLongLong, 0ll)
 };
 
 MpvObject::MpvObject(QObject *owner, const QString &clientName) : QObject(owner)
@@ -171,6 +173,7 @@ MpvObject::MpvObject(QObject *owner, const QString &clientName) : QObject(owner)
         { "audio-bitrate", 0, MPV_FORMAT_DOUBLE },
         { "video-bitrate", 0, MPV_FORMAT_DOUBLE },
         { "paused-for-cache", 0, MPV_FORMAT_FLAG },
+        { "cache-buffering-state", 0, MPV_FORMAT_INT64 },
         { "metadata", 0, MPV_FORMAT_NODE },
         { "audio-device-list", 0, MPV_FORMAT_NODE },
         { "filename", 0, MPV_FORMAT_STRING },

src/mpvwidget.h
@@ -157,6 +157,8 @@ signals:
     void filePathChanged(QString path);
     void subTextChanged(QString subText);
     void hwdecCurrentChanged(QString hwdecCurrent);
+    void pausedForCacheChanged(QString paused);
+    void bufferStateChanged(int64_t percentage);
     void playlistChanged(QVariantList playlist);
 
     void audioTrackSet(int64_t id);

translations/mpc-qt_ar.ts
@@ -1246,6 +1246,10 @@ No action will be triggered.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Remaining time</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1267,6 +1271,10 @@ No action will be triggered.</source>
     </message>
     <message>
         <source> [Freestanding]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Buffering (%1%)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -1331,10 +1339,6 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>Playing</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Buffering</source>
         <translation type="unfinished"></translation>
     </message>
     <message>

translations/mpc-qt_ca.ts
@@ -1390,6 +1390,14 @@ No action will be triggered.</source>
         <translation> ·[Independent]</translation>
     </message>
     <message>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Buffering (%1%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>&amp;Quick Add To Playlist</source>
         <translation>Addició &amp;ràpida a llista de reproducció</translation>
     </message>
@@ -1455,7 +1463,7 @@ No action will be triggered.</source>
     </message>
     <message>
         <source>Buffering</source>
-        <translation>Memòria intermèdia</translation>
+        <translation type="vanished">Memòria intermèdia</translation>
     </message>
     <message>
         <source>Unknown</source>

commit 17ed2ee04e93dc15dd9027775ec0802d6fa73788

manager: Don't change playback state to StoppedState between files
We're actually loading the next file, so show "Loading" instead of
"Stopped".

Fixes: ae2952c0ac66b58bbaa96053231a5c9504a680c4 ("Display buffering
state and progress in status bar on buffer underrun")

src/manager.cpp
@@ -1051,7 +1051,7 @@ void PlaybackManager::mpvw_pausedForCache(QString paused)
     else if (paused == strFalse)
         playbackState_ = PlayingState;
     else
-        playbackState_ = StoppedState;
+        return;
     emit stateChanged(playbackState_, isPlaybackPaused);
 }
 