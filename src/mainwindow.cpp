mpc-qt/mpc-qt

commit 445ca056638ddfa5f9419675ce4b63e7ab4cda60

Trigger Wayland video overflow workaround on first mpv widget paint event
In multi-window mode, it needs to get applied to each window.
Triggering it on first paint instead of on first file start means that
it's fixed on start and works for all media types (including discs and
DVD/BD).

Fixes: a14b1dd80f943c2b61a9f5882a38017c64a05331 ("Work around bug on
Wayland where video doesn't fit window")

src/mainwindow.cpp
@@ -355,6 +355,9 @@ void MainWindow::unfreezeWindow()
 // REMOVEME: work around bug on Wayland where video doesn't fit window
 void MainWindow::fixMpvwSize()
 {
+    firstMpvwPaint = false;
+    if (QGuiApplication::platformName() != "wayland")
+        return;
     QSize size = mpvw->size();
     mpvw->resize(size.width() + 1, size.height() + 1);
     mpvw->resize(size);
@@ -433,6 +436,8 @@ bool MainWindow::eventFilter(QObject *object, QEvent *event)
     bool insideMpv = mpvw ? object == mpvw : false;
     if ((insideMpv || object == playlistWindow_) && event->type() == QEvent::MouseMove) {
         this->mouseMoveEvent(static_cast<QMouseEvent*>(event));
+    } else if (insideMpv && firstMpvwPaint && event->type() == QEvent::Paint && mpvw->isVisible()) {
+        fixMpvwSize();
     }
     if (object == ui->bottomArea) {
         if (event->type() == QEvent::Leave) {

src/main.cpp
@@ -1598,10 +1598,6 @@ void Flow::manager_openingNewFile()
 
 void Flow::manager_aboutToStartPlayingFile()
 {
-    if (firstFile) {
-        firstFile = false;
-        mainWindow->fixMpvwSize();
-    }
     if (rememberFilePosition)
         updateRecentPosition(false);
 }

src/main.h
@@ -156,7 +156,6 @@ private:
 
     static bool isWayland;
     static bool isWaylandMode;
-    bool firstFile = true;
     bool playlistsBackupLoaded = false;
     bool inhibitScreensaver = false;
     bool manipulateScreensaver = false;

src/mainwindow.h
@@ -588,6 +588,7 @@ private:
     QUrl currentFile;
     QString currentFileTitle;
     int previewHeightPercent = 0;
+    bool firstMpvwPaint = true;
 
     IconThemer themer;
     QList<QAction *> menuFavoritesTail;

commit b16b0c947f2614df07c3d0c5a750216cab929db4

mainwindow: Delay Wayland video overflow workaround
In some conditions, it can start an infinite loop when the Paint event
gets
fired asynchronously before we've had a chance to change the
firstMpvwPaint variable.

Fixes: 445ca056638ddfa5f9419675ce4b63e7ab4cda60 ("Trigger Wayland video
overflow workaround on first mpv widget paint event")
Fixes #1091 ("startup crashes, error messages with latest build
(flatpak)").

src/mainwindow.cpp
@@ -345,7 +345,6 @@ void MainWindow::unfreezeWindow()
 // REMOVEME: work around bug on Wayland where video doesn't fit window
 void MainWindow::fixMpvwSize()
 {
-    firstMpvwPaint = false;
     if (QGuiApplication::platformName() != "wayland")
         return;
     QSize size = mpvw->size();
@@ -427,7 +426,8 @@ bool MainWindow::eventFilter(QObject *object, QEvent *event)
     if ((insideMpv || object == playlistWindow_) && event->type() == QEvent::MouseMove) {
         this->mouseMoveEvent(static_cast<QMouseEvent*>(event));
     } else if (insideMpv && firstMpvwPaint && event->type() == QEvent::Paint && mpvw->isVisible()) {
-        fixMpvwSize();
+        firstMpvwPaint = false;
+        QTimer::singleShot(0, this, &MainWindow::fixMpvwSize);
     }
     if (object == ui->bottomArea) {
         if (event->type() == QEvent::Leave) {